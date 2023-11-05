745d846acea22463452e09e58597a26b098121ca
Joel Truher <joel@truher.org>
2023-11-05 08:50:40 -0800

    set up workspace, remove redundant lib

--- comp/swerve100/src/main/java/org/team100/lib/swerve/SwerveKinematicLimits.java
+++
@@ -1,8 +0,0 @@
-package org.team100.lib.swerve;
-
-/** This is from 254 2023 */
-public class SwerveKinematicLimits {
-    public double kMaxDriveVelocity; // m/s
-    public double kMaxDriveAcceleration; // m/s^2
-    public double kMaxSteeringVelocity; // rad/s
-}
