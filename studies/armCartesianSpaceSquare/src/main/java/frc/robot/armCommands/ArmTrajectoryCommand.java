101cd2b515b0e4fb5bcec3cb95b4370559dcd02e
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2023-11-11 17:33:22 -0800

    fix

--- studies/armCartesianSpaceSquare/src/main/java/frc/robot/armCommands/ArmTrajectoryCommand.java
+++ studies/armCartesianSpaceSquare/src/main/java/frc/robot/armCommands/ArmTrajectoryCommand.java
@@ -7,7 +7,7 @@ import frc.robot.subsystems.ArmSubsystem;
 
 public class ArmTrajectoryCommand extends Command {
 
-    private boolean t = false;
+    private boolean definedAngle = true;
     private final ArmSubsystem m_armSubsystem;
     private final double m_startAngle;
     private final double m_endAngle;
@@ -29,23 +29,24 @@ public class ArmTrajectoryCommand extends Command {
 
     public ArmTrajectoryCommand(ArmSubsystem armSubSystem, Translation2d set) {
         this(armSubSystem, set, -1, -1);
-        t = true;
+        definedAngle = false;
     }
 
     @Override
     public void initialize() {
         m_timer.restart();
-        if (t == true) {
-            m_armSubsystem.setTrajectory(m_timer, m_set);
-        } else {
+        if (definedAngle == true) {
             m_armSubsystem.setTrajectory(m_timer,m_set,m_startAngle,m_endAngle);
+        } else {
+            m_armSubsystem.setTrajectory(m_timer,m_set);
         }
 
     }
 
     @Override
     public boolean isFinished() {
-        if (Math.abs(m_armSubsystem.getTrajectoryError().th1) < 0.02 && Math.abs(m_armSubsystem.getTrajectoryError().th2) < 0.02) {
+        double toleranceRadians = 0.02;
+        if (Math.abs(m_armSubsystem.getTrajectoryError().th1) < toleranceRadians && Math.abs(m_armSubsystem.getTrajectoryError().th2) < toleranceRadians) {
             return true;
         }
         return false;
