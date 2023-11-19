390a5b7cb2a675ccc7a1bb0a6299d374a461aa8e
Joel Truher <joel@truher.org>
2023-11-18 21:25:50 -0800

    add log level, not using it yet.

--- lib/src/main/java/org/team100/lib/telemetry/Telemetry.java
+++ lib/src/main/java/org/team100/lib/telemetry/Telemetry.java
@@ -16,14 +16,22 @@ import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.spline.PoseWithCurvature;
 import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.BooleanTopic;
 import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.DoubleArrayTopic;
 import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleTopic;
 import edu.wpi.first.networktables.IntegerPublisher;
+import edu.wpi.first.networktables.IntegerTopic;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.Publisher;
 import edu.wpi.first.networktables.StringArrayPublisher;
+import edu.wpi.first.networktables.StringArrayTopic;
 import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.networktables.StringTopic;
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Simple logging wrapper.
@@ -32,20 +40,40 @@ import edu.wpi.first.wpilibj.DataLogManager;
  * 
  * Don't use a slash for any other reason, e.g. for "meters per second" don't
  * say "m/s" say "m_s"
+ * 
+ * Logged items are "retained" which means they persist even after the logging
+ * stops; this means you should see the latest values after disabling the robot.
  */
 public class Telemetry {
+    public enum Level {
+        /** useful for normal operations */
+        INFO,
+        /** primarily useful during development */
+        DEBUG
+    }
+
     /**
-     * useful for troubleshooting unit tests.  it's quite slow.
+     * useful for troubleshooting unit tests. it's quite slow.
      */
     private static final boolean kAlsoPrint = false;
     private static final Telemetry instance = new Telemetry();
     private final NetworkTableInstance inst;
     private final Map<String, Publisher> pubs;
+    private final SendableChooser<Level> m_manualModeChooser;
 
     /** Uses the default network table instance. */
     public Telemetry() {
         inst = NetworkTableInstance.getDefault();
         pubs = new HashMap<>();
+        m_manualModeChooser = new SendableChooser<>();
+        for (Level level : Level.values()) {
+            m_manualModeChooser.addOption(level.name(), level);
+        }
+        m_manualModeChooser.setDefaultOption(
+                Level.DEBUG.name(),
+                Level.DEBUG);
+        SmartDashboard.putData(m_manualModeChooser);
+
         DataLogManager.start();
     }
 
@@ -56,37 +84,61 @@ public class Telemetry {
     public void log(String key, boolean val) {
         if (kAlsoPrint)
             System.out.println(key + ": " + val);
-        pub(key, k -> inst.getBooleanTopic(k).publish(), BooleanPublisher.class).set(val);
+        pub(key, k -> {
+            BooleanTopic t = inst.getBooleanTopic(k);
+            t.setRetained(true);
+            return t.publish();
+        }, BooleanPublisher.class).set(val);
     }
 
     public void log(String key, double val) {
         if (kAlsoPrint)
             System.out.println(key + ": " + val);
-        pub(key, k -> inst.getDoubleTopic(k).publish(), DoublePublisher.class).set(val);
+        pub(key, k -> {
+            DoubleTopic t = inst.getDoubleTopic(k);
+            t.setRetained(true);
+            return t.publish();
+        }, DoublePublisher.class).set(val);
     }
 
     public void log(String key, double[] val) {
         if (kAlsoPrint)
             System.out.println(key + ": " + val);
-        pub(key, k -> inst.getDoubleArrayTopic(k).publish(), DoubleArrayPublisher.class).set(val);
+        pub(key, k -> {
+            DoubleArrayTopic t = inst.getDoubleArrayTopic(k);
+            t.setRetained(true);
+            return t.publish();
+        }, DoubleArrayPublisher.class).set(val);
     }
 
     public void log(String key, long val) {
         if (kAlsoPrint)
             System.out.println(key + ": " + val);
-        pub(key, k -> inst.getIntegerTopic(k).publish(), IntegerPublisher.class).set(val);
+        pub(key, k -> {
+            IntegerTopic t = inst.getIntegerTopic(k);
+            t.setRetained(true);
+            return t.publish();
+        }, IntegerPublisher.class).set(val);
     }
 
     public void log(String key, String val) {
         if (kAlsoPrint)
             System.out.println(key + ": " + val);
-        pub(key, k -> inst.getStringTopic(k).publish(), StringPublisher.class).set(val);
+        pub(key, k -> {
+            StringTopic t = inst.getStringTopic(k);
+            t.setRetained(true);
+            return t.publish();
+        }, StringPublisher.class).set(val);
     }
 
     public void log(String key, String[] val) {
         if (kAlsoPrint)
             System.out.println(key + ": " + val);
-        pub(key, k -> inst.getStringArrayTopic(k).publish(), StringArrayPublisher.class).set(val);
+        pub(key, k -> {
+            StringArrayTopic t = inst.getStringArrayTopic(k);
+            t.setRetained(true);
+            return t.publish();
+        }, StringArrayPublisher.class).set(val);
     }
 
     public void log(String key, Pose2d val) {
@@ -153,11 +205,5 @@ public class Telemetry {
         if (key.charAt(0) != '/')
             throw new IllegalArgumentException("no leading slash");
         return key;
-    }
-
-    /** For testing. */
-    Telemetry(NetworkTableInstance inst, Map<String, Publisher> pubs) {
-        this.inst = inst;
-        this.pubs = pubs;
     }
 }
