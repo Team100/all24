6c1c9ad0cb7dfdb7f3abd38d3790bdacb22b347d
Joel Truher <joel@truher.org>
2023-12-17 19:18:57 -0800

    fix the build

--- lib/src/test/java/org/team100/lib/commands/drivetrain/RotateTest.java
+++ lib/src/test/java/org/team100/lib/commands/drivetrain/RotateTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.team100.lib.controller.HolonomicDriveController3;
 import org.team100.lib.geometry.GeometryUtil;
 import org.team100.lib.motion.drivetrain.MockSwerveDriveSubsystem;
 import org.team100.lib.motion.drivetrain.SpeedLimits;
@@ -17,7 +16,7 @@ import edu.wpi.first.wpilibj.simulation.SimHooks;
 
 /** Example of mock objects for testing. */
 class RotateTest {
-    private static final double kDelta = 0.001;
+    private static final double kDelta = 0.02;
 
     @Test
     void testRotate() {
@@ -49,14 +48,14 @@ class RotateTest {
 
         SimHooks.stepTiming(1);
         rotate.execute();
-        assertEquals(0.5, rotate.refTheta.getX(), 0.01);
-        assertEquals(2.51, swerveDriveSubsystem.twist.dtheta, 0.01);
+        assertEquals(0.5, rotate.refTheta.getX(), kDelta);
+        assertEquals(2.75, swerveDriveSubsystem.twist.dtheta, kDelta);
 
         SimHooks.stepTiming(1);
         swerveDriveSubsystem.pose = new Pose2d(0, 0, new Rotation2d(1));
         rotate.execute();
         assertEquals(1.408, rotate.refTheta.getX(), kDelta);
-        assertEquals(1.813, swerveDriveSubsystem.twist.dtheta, kDelta);
+        assertEquals(2, swerveDriveSubsystem.twist.dtheta, kDelta);
 
         SimHooks.stepTiming(1);
         swerveDriveSubsystem.pose = new Pose2d(0, 0, new Rotation2d(Math.PI));
