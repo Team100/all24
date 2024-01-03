66a85219db424ba6f3f3fba1a6d814b2d649c2ca
Joel Truher <joel@truher.org>
2024-01-02 17:16:05 -0800

    illustrate simhook notifier segv

---
+++ studies/simhooks_segv/src/test/java/org/team100/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.team100;
+
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.wpilibj.simulation.NotifierSim;
+
+class ExampleTest {
+    @Test
+    void testTiming() {
+        NotifierSim.getNumNotifiers();
+
+    }
+}
