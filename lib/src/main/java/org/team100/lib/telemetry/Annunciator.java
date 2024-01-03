11be18d9b394ed64bf904b368d88e94f7a52d7b9
Joel Truher <joel@truher.org>
2024-01-02 20:14:32 -0800

    use a digital output instead

--- lib/src/main/java/org/team100/lib/telemetry/Annunciator.java
+++ lib/src/main/java/org/team100/lib/telemetry/Annunciator.java
@@ -1,29 +1,24 @@
 package org.team100.lib.telemetry;
 
 import edu.wpi.first.util.function.BooleanConsumer;
-import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.DigitalOutput;
 
 /**
- * Turns on a PWM in response to boolean alerts, e.g. from the Monitor class.
- * 
- * I'd suggest wiring the PWM through a motor controller to a car horn.
+ * Turns on a digital output in response to boolean alerts, e.g. from the
+ * Monitor class.
  */
 public class Annunciator implements BooleanConsumer {
-    private final PWM m_pwm;
+    private final DigitalOutput m_pwm;
     private final AnnunciatorVisualization m_viz;
 
     public Annunciator(int channel) {
-        m_pwm = new PWM(channel);
+        m_pwm = new DigitalOutput(channel);
         m_viz = new AnnunciatorVisualization();
     }
 
     @Override
     public void accept(boolean value) {
-        if (value) {
-            m_pwm.setSpeed(1);
-        } else {
-            m_pwm.setSpeed(0);
-        }
+        m_pwm.set(value);
         m_viz.set(value);
     }
 }
