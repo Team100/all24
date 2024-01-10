fe273b2b0b1922bf3193a476b0d4cda1914101b1
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-01-10 11:49:11 -0800

    Created new file for gamepiece finder

--- comp/vision/gamepiece_finder24.py
+++ comp/vision/gamepiece_finder24.py
@@ -13,97 +13,44 @@ import math
 class GamePieceFinder:
 
     def __init__(self, topic_name, camera_params):
-        self.purple_lower = (150, 100, 0)
-        self.purple_higher = (190, 255, 255)
-        self.yellow_lower = (15, 100, 0)
-        self.yellow_higher = (100, 255, 255)
+        self.object_lower = (0 , 0, 200)
+        self.object_higher = (255, 170, 255)
         self.scale_factor = 1
         self.width = camera_params[0]
         self.height = camera_params[1]
-        self.cube_height = .105
-        self.cone_height = .105
+        self.object_height = .105
         self.theta = 0
         self.topic_name = topic_name
-        self.initialize_nt()
+        self.initialize_nt()    
         self.output_stream = CameraServer.putVideo("Processed", self.width, self.height)
 
     def initialize_nt(self):
         """Start NetworkTables with Rio as server, set up publisher."""
-        inst = NetworkTableInstance.getDefault()
-        inst.startClient4("retro-finder")
+        self.inst = NetworkTableInstance.getDefault()
+        self.inst.startClient4("retro-finder")
         # this is always the RIO IP address; set a matching static IP on your
         # laptop if you're using this in simulation.
-        inst.setServer("10.1.0.2")
+        self.inst.setServer("10.1.0.2")
         # Table for vision output information
-        self.vision_nt = inst.getTable("Vision")
+        self.vision_nt = self.inst.getTable("Vision")
         self.vision_nt_msgpack = self.vision_nt.getRawTopic(self.topic_name).publish(
             "msgpack"
         )
 
-    def find_cubes(self, img):
-        # 15, 100, 0
-        # 40, 255, 255
-        #120, 100, 0
-        # 145, 255, 255
-        purple_range = cv2.inRange(img, self.purple_lower, self.purple_higher)
+    def find_object(self, img):
+        range = cv2.inRange(img, self.object_lower, self.object_higher)
         img_rgb = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
-        floodfill = purple_range.copy()
-        h, w = purple_range.shape[:2]
+        floodfill = range.copy()
+        h, w = range.shape[:2]
         mask = np.zeros((h+2, w+2), np.uint8)
         cv2.floodFill(floodfill, mask, (0,0), 255)
         floodfill_inv = cv2.bitwise_not(floodfill)
-        img_floodfill = purple_range | floodfill_inv
-        median = cv2.medianBlur(purple_range, 5)
-        contours, hierarchy = cv2.findContours(
-            median, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cubes = {}
-        cubes["cubes"] = []
-        for c in contours:
-            _, _, cnt_width, cnt_height = cv2.boundingRect(c)
-            if (cnt_height < 50):
-                continue
-            if (cnt_height/cnt_width < .5):
-                continue
-            if (cnt_height/cnt_width > 2):
-                continue
-            mmnts = cv2.moments(c)
-            if (mmnts["m00"] == 0):
-                continue
-
-            cX = int(mmnts["m10"] / mmnts["m00"])
-            cY = int(mmnts["m01"] / mmnts["m00"])
-
-            translation_x = (cX-self.width/2)* \
-                (self.cube_height*math.cos(self.theta)/cnt_height)
-            translation_y = (cY-self.height/2) * \
-                (self.cube_height*math.cos(self.theta)/cnt_height)
-            translation_z = (self.cube_height*self.scale_factor*math.cos(self.theta))/(cnt_height)
-
-            cube = [translation_x, translation_y, translation_z]
-
-            cubes["cubes"].append(
-                {
-                    "pose_t": cube
-                }
-            )
-        #     self.draw_result(img_rgb, c, cX, cY, cube)
-        # self.output_stream.putFrame(img_rgb)
-        return cubes
-    
-    def find_cones(self, img):
-        yellow_range = cv2.inRange(img, self.yellow_lower, self.yellow_higher)
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
-        floodfill = yellow_range.copy()
-        h, w = yellow_range.shape[:2]
-        mask = np.zeros((h+2, w+2), np.uint8)
-        cv2.floodFill(floodfill, mask, (0,0), 255)
-        floodfill_inv = cv2.bitwise_not(floodfill)
-        img_floodfill = yellow_range | floodfill_inv
+        img_floodfill = range | floodfill_inv
         median = cv2.medianBlur(img_floodfill, 5)
         contours, hierarchy = cv2.findContours(
             median, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cones = {}
-        cones["cones"] = []
+        objects = {}
+        objects["objects"] = []
         for c in contours:
             _, _, cnt_width, cnt_height = cv2.boundingRect(c)
             if (cnt_height < 50):
@@ -118,23 +65,25 @@ class GamePieceFinder:
 
             cX = int(mmnts["m10"] / mmnts["m00"])
             cY = int(mmnts["m01"] / mmnts["m00"])
-
+            self.vision_nt_msgpack.set(cX)
+            self.vision_nt_msgpack.set(cY)
             translation_x = (cX-self.width/2)* \
-                (self.cone_height*math.cos(self.theta)/cnt_height)
+                (self.object_height*math.cos(self.theta)/cnt_height)
             translation_y = (cY-self.height/2) * \
-                (self.cone_height*math.cos(self.theta)/cnt_height)
-            translation_z = (self.cone_height*self.scale_factor*math.cos(self.theta))/(cnt_height)
+                (self.object_height*math.cos(self.theta)/cnt_height)
+            translation_z = (self.object_height*self.scale_factor*math.cos(self.theta))/(cnt_height)
 
-            cone = [translation_x, translation_y, translation_z]
+            object = [translation_x, translation_y, translation_z]
 
-            cones["cones"].append(
+            objects["objects"].append(
                 {
-                    "pose_t": cone
+                    "pose_t": object
                 }
             )
-            self.draw_result(img_rgb, c, cX, cY, cone)
+            self.draw_result(img_rgb, c, cX, cY, object)
+        self.inst.flush()
         self.output_stream.putFrame(img_rgb)
-        return cones
+        return objects
 
     def draw_result(self, img, cnt, cX, cY, piece):
         float_formatter = {"float_kind": lambda x: f"{x:4.1f}"}
@@ -154,23 +103,15 @@ class GamePieceFinder:
         img_bgr = img_bgr[201:401,:,:]
         img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
         img_hsv = np.ascontiguousarray(img_hsv)
-        cubes = self.find_cubes(img_hsv)
-        cones = self.find_cones(img_hsv)
+        objects = self.find_object(img_hsv)
         pieces = {}
         pieces["pieces"] = []
         pieces["pieces"].append(
                 {
-                    "cubes": cubes
-                }
-            )
-        pieces["pieces"].append(
-                {
-                    "cones": cones
+                    "objects": objects
                 }
             )
         posebytes = msgpack.packb(pieces)
-
-
         self.vision_nt_msgpack.set(posebytes)
 
 def main():
