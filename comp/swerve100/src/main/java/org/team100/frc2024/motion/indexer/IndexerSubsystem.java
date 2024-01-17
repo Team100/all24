289d4389fa7203788563d2abcc77e315db2a67d7
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-01-16 18:50:59 -0800

    Got rid of trapezoid profile

--- comp/swerve100/src/main/java/org/team100/frc2024/motion/indexer/IndexerSubsystem.java
+++ comp/swerve100/src/main/java/org/team100/frc2024/motion/indexer/IndexerSubsystem.java
@@ -28,7 +28,6 @@ public class IndexerSubsystem extends SubsystemBase {
         m_servo.periodic();
         }
     public void set(double value) {
-        setpoint = m_trapezoid.calculate(0.02, setpoint, new State100(value, 0, 0));
         m_servo.setVelocity(setpoint.x());
     }
 
