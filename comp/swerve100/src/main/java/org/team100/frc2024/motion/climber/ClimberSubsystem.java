fa89ab7cf2b8090bc703ede29e85fa7efa4bd6ea
Joel Truher <joel@truher.org>
2024-01-17 08:37:09 -0800

    move servo instantiation to factory

--- comp/swerve100/src/main/java/org/team100/frc2024/motion/climber/ClimberSubsystem.java
+++ comp/swerve100/src/main/java/org/team100/frc2024/motion/climber/ClimberSubsystem.java
@@ -1,27 +1,41 @@
 package org.team100.frc2024.motion.climber;
 
-import org.team100.lib.encoder.drive.NeoDriveEncoder;
 import org.team100.lib.motion.components.PositionServo;
-import org.team100.lib.motion.components.SelectableVelocityServo;
-import org.team100.lib.motion.components.VelocityServo;
-import org.team100.lib.motor.drive.NeoDriveMotor;
-import org.team100.lib.profile.TrapezoidProfile100;
+import org.team100.lib.motion.components.ServoFactory;
 import org.team100.lib.units.Distance;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
     private static final double kGearRatio = 20.0;
     private static final double kWinchDiameterM = 0.01;
+    private static final double kMaxVelM_S = 1;
+    private static final double kMaxAccelM_S2 = 1;
+    /** Position servo PID kP */
+    private static final double kP = 1;
 
     private final PositionServo<Distance> s1;
     private final PositionServo<Distance> s2;
 
     public ClimberSubsystem(String name1, String name2, int canID1, int canID2) {
-        s1 = newPositionServo(name1, canID1, false);
-        s2 = newPositionServo(name2, canID2, true);
+        s1 = ServoFactory.neoPositionServo(
+                name1,
+                canID1,
+                false,
+                kGearRatio,
+                kWinchDiameterM,
+                kMaxVelM_S,
+                kMaxAccelM_S2,
+                kP);
+        s2 = ServoFactory.neoPositionServo(
+                name2,
+                canID2,
+                true,
+                kGearRatio,
+                kWinchDiameterM,
+                kMaxVelM_S,
+                kMaxAccelM_S2,
+                kP);
     }
 
     /** Set velocity in meters per second */
@@ -39,39 +53,5 @@ public class ClimberSubsystem extends SubsystemBase {
     public void periodic() {
         s1.periodic();
         s2.periodic();
-    }
-
-    /**
-     * Using the velocity servo means we can select onboard or outboard velocity
-     * closed-loop, and control position with the position servo.
-     */
-    private static PositionServo<Distance> newPositionServo(
-            String name,
-            int canId,
-            boolean motorPhase) {
-        NeoDriveMotor motor = new NeoDriveMotor(
-                name,
-                canId,
-                motorPhase,
-                kGearRatio,
-                kWinchDiameterM);
-        NeoDriveEncoder encoder = new NeoDriveEncoder(
-                name + "/encoder",
-                motor,
-                kWinchDiameterM * Math.PI);
-        VelocityServo<Distance> vServo = new SelectableVelocityServo<>(
-                name + "/velocity",
-                motor,
-                encoder,
-                new PIDController(1, 0, 0),
-                new SimpleMotorFeedforward(0, 1));
-        return new PositionServo<>(
-                name + "/position",
-                vServo,
-                encoder,
-                1,
-                new PIDController(1, 0, 0),
-                new TrapezoidProfile100(1, 1, 0.05),
-                Distance.instance);
     }
 }
