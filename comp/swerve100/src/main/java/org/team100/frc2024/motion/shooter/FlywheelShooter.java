578acfe13c45704efa16da15e127111acd44da25
anorakthegreat <sanjanksen4@gmail.com>
2024-01-18 02:32:17 -0800

    added trapezoid profile in the position servo and added position servos for all the subsystems. added support for switching subsystems. need to integrate with Identity.

--- comp/swerve100/src/main/java/org/team100/frc2024/motion/shooter/FlywheelShooter.java
+++ comp/swerve100/src/main/java/org/team100/frc2024/motion/shooter/FlywheelShooter.java
@@ -1,25 +1,26 @@
-package org.team100.frc2024.motion.climber;
+package org.team100.frc2024.motion.shooter;
 
+import org.team100.lib.controller.State100;
 import org.team100.lib.motion.components.PositionServo;
 import org.team100.lib.motion.components.ServoFactory;
+
 import org.team100.lib.units.Distance;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimberSubsystem extends SubsystemBase {
+public class FlywheelShooter extends ShooterFactory {
     private static final double kGearRatio = 20.0;
     private static final double kWinchDiameterM = 0.01;
-    private static final double kMaxVelM_S = 1;
-    private static final double kMaxAccelM_S2 = 1;
-    /** Position servo PID kP */
-    private static final double kP = 1;
+    private static final double kMaxVelM_S = 8;
+    private static final double kMaxAccelM_S2 = 30;
 
-    private final PositionServo<Distance> s1;
-    private final PositionServo<Distance> s2;
+    private final PositionServo<Distance> leftShooter;
+    private final PositionServo<Distance> rightShooter;
 
-    public ClimberSubsystem(String name1, String name2, int canID1, int canID2) {
-        s1 = ServoFactory.neoPositionServo(
+    public FlywheelShooter(String name1, String name2, int canID1, int canID2) {
+
+        leftShooter = ServoFactory.neoPositionServo(
                 name1,
                 canID1,
                 false,
@@ -28,31 +29,27 @@ public class ClimberSubsystem extends SubsystemBase {
                 kMaxVelM_S,
                 kMaxAccelM_S2,
                 new PIDController(1, 0, 0));
-        s2 = ServoFactory.neoPositionServo(
+
+        rightShooter = ServoFactory.neoPositionServo(
                 name2,
                 canID2,
-                true,
+                false,
                 kGearRatio,
                 kWinchDiameterM,
                 kMaxVelM_S,
                 kMaxAccelM_S2,
                 new PIDController(1, 0, 0));
+        
     }
 
-    /** Set velocity in meters per second */
-    public void set(double value) {
-        s1.setVelocity(value);
-        s2.setVelocity(value);
-    }
-
-    public void setPosition(double value) {
-        s1.setPosition(value);
-        s2.setPosition(value);
+    public void setVelocity(double value) {
+        leftShooter.setVelocity(value);
+        rightShooter.setVelocity(value);
     }
 
     @Override
     public void periodic() {
-        s1.periodic();
-        s2.periodic();
+        leftShooter.periodic();
+        rightShooter.periodic();
     }
 }
