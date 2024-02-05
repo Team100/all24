7d92e34e4777475c9341aea234fc3b1fd0c35d8d
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-02-04 19:39:18 -0800

    More stuff

--- lib/src/test/java/org/team100/lib/localization/TestCameraAngles.java
+++ lib/src/test/java/org/team100/lib/localization/TestCameraAngles.java
@@ -1,18 +1,17 @@
-package org.team100.lib.testing;
+package org.team100.lib.localization;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.team100.lib.util.CameraAngles;
 
-class TrapezoidProfile100Test {
-
-    /** Now we expose acceleration in the profile state, so make sure it's right. */
+class TestCameraAngles {
     @Test
     void testCameraAngles() {
         {
             CameraAngles camera = new CameraAngles(5, 67.5, 50, 832, 616, 1);
                assertEquals(0,camera.getX(416,200),0.0001);
+               assertEquals(0.7,camera.getY(0),0.001);
         }
     }
 }
