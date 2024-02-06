a20dcc6115bc70a3ad691a95bbeaf383c6780885
Joel Truher <joel@truher.org>
2024-02-06 12:33:09 -0800

    added falcon factory, set to falcon 6 for comp bot

--- lib/src/main/java/org/team100/lib/experiments/Experiment.java
+++ lib/src/main/java/org/team100/lib/experiments/Experiment.java
@@ -4,15 +4,25 @@ package org.team100.lib.experiments;
  * An experiment is something that can be selectively enabled.
  */
 public enum Experiment {
-    /** Smooth chassis speeds. */
+    /**
+     * Smooth chassis speeds.
+     */
     UseSetpointGenerator,
-    /** Offload simple servo velocity to the controller */
+    /**
+     * Offload simple servo velocity to the controller
+     */
     UseClosedLoopVelocity,
-    /** Use initial state in trajectory generation */
+    /**
+     * Use initial state in trajectory generation
+     */
     UseInitialVelocity,
-    /** For the oscillate command, drive modules directly */
+    /**
+     * For the oscillate command, drive modules directly
+     */
     OscillateDirect,
-    /** For the oscillate command, drive rotation instead of linearly */
+    /**
+     * For the oscillate command, drive rotation instead of linearly
+     */
     OscillateTheta,
     /**
      * Flush network tables as often as possible. Do not enable this experiment in
