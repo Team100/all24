67354b53d13b36421db526a0a0a2d095450edf7a
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-02-07 00:29:30 -0800

    I did a lot of math

--- comp/swerve100/src/main/java/org/team100/frc2024/motion/drivetrain/ShooterUtil.java
+++ comp/swerve100/src/main/java/org/team100/frc2024/motion/drivetrain/ShooterUtil.java
@@ -7,19 +7,21 @@ package org.team100.frc2024.motion.drivetrain;
 import org.team100.frc2024.FieldConstants;
 import org.team100.lib.motion.drivetrain.SwerveState;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class ShooterUtil {
 
     public static Translation2d getOffsetTranslation(SwerveState state, double shooterVelocity){
         double distanceHorizontal = state.y().v();
-
-        double horizontalOffsetDistance = FieldConstants.SHOOTER_CENTER_Y + -1.0*  distanceHorizontal / shooterVelocity * (Math.abs(state.translation().getY()-FieldConstants.SHOOTER_CENTER_Y));
-
+        double speakerHeight = 2;
+        Translation2d displacement = new Translation2d(FieldConstants.SHOOTER_CENTER_X, FieldConstants.SHOOTER_CENTER_Y).minus(state.translation());
+        double angleOfElevation = Math.atan2(speakerHeight, displacement.getNorm());
+        double horzVelocity = Math.cos(angleOfElevation)*shooterVelocity;
+        double time = displacement.getNorm()/horzVelocity;
+        double horizontalOffsetDistance = FieldConstants.SHOOTER_CENTER_Y + -1.0 * distanceHorizontal * time;
         double distanceVertical = state.x().v();
 
-        double verticalOffsetDistance = FieldConstants.SHOOTER_CENTER_X + -1.0 * distanceVertical / shooterVelocity * (Math.abs(state.translation().getX()-FieldConstants.SHOOTER_CENTER_X));
+        double verticalOffsetDistance = FieldConstants.SHOOTER_CENTER_X + -1.0 * distanceVertical * time;
 
         return new Translation2d(verticalOffsetDistance, horizontalOffsetDistance);
 
