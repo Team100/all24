7ae1e8f4bb9c58946f206f7192a393a61d1b403b
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-02-08 17:07:11 -0800

    Fixed

--- lib/src/main/java/org/team100/lib/motor/drive/Falcon6DriveMotor.java
+++ lib/src/main/java/org/team100/lib/motor/drive/Falcon6DriveMotor.java
@@ -12,7 +12,6 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
--- lib/src/main/java/org/team100/lib/motor/turning/Falcon6TurningMotor.java
+++ lib/src/main/java/org/team100/lib/motor/turning/Falcon6TurningMotor.java
@@ -13,7 +13,7 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -192,10 +192,9 @@ public class Falcon6TurningMotor implements Motor100<Angle100> {
         double accelFF = accelFF(accelRad_S_S);
         double kFF = frictionFF + velocityFF + accelFF;
 
-        VelocityVoltage v = new VelocityVoltage(motorRev_S);
+        VelocityTorqueCurrentFOC v = new VelocityTorqueCurrentFOC(motorRev_S);
         v.FeedForward = kFF;
         v.Acceleration = motorRev_S2;
-        v.EnableFOC = true;
         m_motor.setControl(v);
 
         // m_motor.set(ControlMode.Velocity, motorTick_100ms,
