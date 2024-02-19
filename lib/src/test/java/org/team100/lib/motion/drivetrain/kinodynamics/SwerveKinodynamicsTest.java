bece52871058b51b81f5fff49486843409230a0f
truher <joel@truher.org>
2024-02-19 15:15:11 -0800

    remove more print statements

--- lib/src/test/java/org/team100/lib/motion/drivetrain/kinodynamics/SwerveKinodynamicsTest.java
+++ lib/src/test/java/org/team100/lib/motion/drivetrain/kinodynamics/SwerveKinodynamicsTest.java
@@ -391,32 +391,32 @@ class SwerveKinodynamicsTest {
             double x2 = Math.abs(i2.vxMetersPerSecond);
             double x1 = Math.abs(i1.vxMetersPerSecond);
             if (x2 > x1 + 1e-6) {
-                System.out.printf("X high %.8f %.8f\n", x2, x1);
+                // System.out.printf("X high %.8f %.8f\n", x2, x1);
                 dump(i, s, i1, i2);
             }
             // but i1 shouldn't be *too* much faster.
             if ((x2 - x1) / x1 > 0.1) {
-                System.out.printf("X low %.8f %.8f\n", x2, x1);
+                // System.out.printf("X low %.8f %.8f\n", x2, x1);
                 dump(i, s, i1, i2);
             }
             double y2 = Math.abs(i2.vyMetersPerSecond);
             double y1 = Math.abs(i1.vyMetersPerSecond);
             if (y2 > y1 + 1e-6) {
-                System.out.printf("Y high %.8f %.8f\n", y2, y1);
+                // System.out.printf("Y high %.8f %.8f\n", y2, y1);
                 dump(i, s, i1, i2);
             }
             if ((y2 - y1) / y1 > 0.1) {
-                System.out.printf("Y low %.8f %.8f\n", y2, y1);
+                // System.out.printf("Y low %.8f %.8f\n", y2, y1);
                 dump(i, s, i1, i2);
             }
             double o2 = Math.abs(i2.omegaRadiansPerSecond);
             double o1 = Math.abs(i1.omegaRadiansPerSecond);
             if (o2 > o1 + 1e-6) {
-                System.out.printf("omega high %.8f %.8f\n", o2, o1);
+                // System.out.printf("omega high %.8f %.8f\n", o2, o1);
                 dump(i, s, i1, i2);
             }
             if ((o2 - o1) / o1 > 0.1) {
-                System.out.printf("omega low %.8f %.8f\n", o2, o1);
+                // System.out.printf("omega low %.8f %.8f\n", o2, o1);
                 dump(i, s, i1, i2);
             }
         }
@@ -446,32 +446,32 @@ class SwerveKinodynamicsTest {
             double x2 = Math.abs(i2.vxMetersPerSecond);
             double x1 = Math.abs(i1.vxMetersPerSecond);
             if (x2 > x1 + 1e-6) {
-                System.out.printf("X high %.8f %.8f\n", x2, x1);
+                // System.out.printf("X high %.8f %.8f\n", x2, x1);
                 dump(i, s, i1, i2);
             }
             // but i1 shouldn't be *too* much faster.
             if ((x2 - x1) / x1 > 0.1) {
-                System.out.printf("X low %.8f %.8f\n", x2, x1);
+                // System.out.printf("X low %.8f %.8f\n", x2, x1);
                 dump(i, s, i1, i2);
             }
             double y2 = Math.abs(i2.vyMetersPerSecond);
             double y1 = Math.abs(i1.vyMetersPerSecond);
             if (y2 > y1 + 1e-6) {
-                System.out.printf("Y high %.8f %.8f\n", y2, y1);
+                // System.out.printf("Y high %.8f %.8f\n", y2, y1);
                 dump(i, s, i1, i2);
             }
             if ((y2 - y1) / y1 > 0.1) {
-                System.out.printf("Y low %.8f %.8f\n", y2, y1);
+                // System.out.printf("Y low %.8f %.8f\n", y2, y1);
                 dump(i, s, i1, i2);
             }
             double o2 = Math.abs(i2.omegaRadiansPerSecond);
             double o1 = Math.abs(i1.omegaRadiansPerSecond);
             if (o2 > o1 + 1e-6) {
-                System.out.printf("omega high %.8f %.8f\n", o2, o1);
+                // System.out.printf("omega high %.8f %.8f\n", o2, o1);
                 dump(i, s, i1, i2);
             }
             if ((o2 - o1) / o1 > 0.1) {
-                System.out.printf("omega low %.8f %.8f\n", o2, o1);
+                // System.out.printf("omega low %.8f %.8f\n", o2, o1);
                 dump(i, s, i1, i2);
             }
         }
