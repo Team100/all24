894838952170e4d87989a65c75f974e09fbc6f22
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-02-29 17:44:30 -0800

    Tunned values

--- lib/src/main/java/org/team100/lib/config/SimulatedCamera.java
+++ lib/src/main/java/org/team100/lib/config/SimulatedCamera.java
@@ -3,7 +3,6 @@ package org.team100.lib.config;
 import java.util.ArrayList;
 import java.util.Optional;
 
-import org.team100.lib.localization.PoseEstimationHelper;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
