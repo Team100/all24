5caa732ce8125d790a1348a4449ce5056c41bbba
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-03-08 12:34:18 -0800

    Bugfix

--- comp/swerve100/src/main/java/org/team100/frc2024/motion/StowAmpCommand.java
+++ comp/swerve100/src/main/java/org/team100/frc2024/motion/StowAmpCommand.java
@@ -39,7 +39,6 @@ public class StowAmpCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    System.out.println("STOW HAS FINISHED");
     finished = false;
   }
 
