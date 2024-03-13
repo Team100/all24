4be7185f94bbec5705c0945d5e3084f7e53246c7
Joel Truher <joel@truher.org>
2024-03-13 11:27:02 -0700

    made LEDs show the alliance color, and flash

--- lib/src/main/java/org/team100/lib/indicator/Patterns.java
+++ lib/src/main/java/org/team100/lib/indicator/Patterns.java
@@ -4,48 +4,56 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class Patterns {
-
-
-     public static void solid(LEDStrip strip, AddressableLEDBuffer buffer, Color s){
-        for(var i = strip.getOffset(); i < strip.getLength(); i++){
-            buffer.setLED(i, s);
+    /**
+     * Fill buffer corresponding to the specified strip with the specified color
+     */
+    public static void solid(LEDStrip strip, AddressableLEDBuffer buffer, Color color) {
+        for (var i = strip.getOffset(); i < strip.getLength(); i++) {
+            buffer.setLED(i, color);
         }
-     }
+    }
 
-    public static void rainbow(LEDStrip strip, AddressableLEDBuffer buffer) {
-
+    /**
+     * work-in-progress
+     */
+    static void rainbow(LEDStrip strip, AddressableLEDBuffer buffer) {
         int m_rainbowFirstPixelHue = 0;
-
         // For every pixel
         for (var i = strip.getOffset(); i < strip.getLength(); i++) {
-          // Calculate the hue - hue is easier for rainbows because the color
-          // shape is a circle so only one value needs to precess
-          final var hue = (m_rainbowFirstPixelHue + (i * 180 / buffer.getLength())) % 180;
-          // Set the value
-          buffer.setHSV(i, hue, 255, 128);
+            // Calculate the hue - hue is easier for rainbows because the color
+            // shape is a circle so only one value needs to precess
+            final var hue = (m_rainbowFirstPixelHue + (i * 180 / buffer.getLength())) % 180;
+            // Set the value
+            buffer.setHSV(i, hue, 255, 128);
         }
         // Increase by to make the rainbow "move"
         m_rainbowFirstPixelHue += 3;
         // Check bounds
         m_rainbowFirstPixelHue %= 180;
-      }
+    }
 
-      public static void chase(Color[] colors, LEDStrip strip, AddressableLEDBuffer buffer) {
-
+    /**
+     * work-in-progress
+     */
+    static void chase(Color[] colors, LEDStrip strip, AddressableLEDBuffer buffer) {
         int numberOfColors = colors.length;
-		int effectiveIndex;
-		int colorIndex;
+        int effectiveIndex;
+        int colorIndex;
         int offset = 0;
-		int bufferLength = buffer.getLength();
+        int bufferLength = buffer.getLength();
         int segmentWidth = 0;
-        
-		for (int index = 0; index < bufferLength; index++){
-			effectiveIndex = (index + offset) % bufferLength;
-			colorIndex =( index /segmentWidth )% numberOfColors;
-			buffer.setLED(effectiveIndex, colors[colorIndex]);
-		}
 
-		offset =(offset+1) %bufferLength;
+        for (int index = 0; index < bufferLength; index++) {
+            effectiveIndex = (index + offset) % bufferLength;
+            colorIndex = (index / segmentWidth) % numberOfColors;
+            buffer.setLED(effectiveIndex, colors[colorIndex]);
+        }
 
-      }
+        offset = (offset + 1) % bufferLength;
+
+    }
+
+    private Patterns() {
+        //
+    }
 }
