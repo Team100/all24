8c0ad6e6ee7b91ac9ea778a7d6f324f2b262c5dd
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-03-26 16:46:41 -0700

    Updarted camera serial numbers

--- comp/vision/tag_finder24.py
+++ comp/vision/tag_finder24.py
@@ -32,13 +32,13 @@ class Camera(Enum):
     """Keep this synchronized with java team100.config.Camera."""
     # TODO get correct serial numbers for Delta
     A = "10000000caeaae82"  # "BETA FRONT"
-    B = "1000000013c9c96c"  # "BETA BACK"
+    # B = "1000000013c9c96c"  # "BETA BACK"
     C = "10000000a7c673d9"  # "GAMMA INTAKE"
 
-    SHOOTER = "100000004e0a1fb9"  # "DELTA SHOOTER"
-    RIGHTAMP = "1000000031b9d05b"  # "DELTA AMP-PLACER"
-    LEFTAMP = "1000000031b9d05b"  # "DELTA AMP-PLACER"
-    GAME_PIECE = "10000000a7c673dc"  # "DELTA INTAKE"
+    SHOOTER = "10000000a7a892c0"  # "DELTA SHOOTER"
+    RIGHTAMP = "10000000caeaae82"  # "DELTA AMP-PLACER"
+    LEFTAMP = "100000004e0a1fb9"  # "DELTA AMP-PLACER"
+    GAME_PIECE = "1000000013c9c96c"  # "DELTA INTAKE"
 
     G = "10000000a7a892c0"  # ""
     UNKNOWN = None
