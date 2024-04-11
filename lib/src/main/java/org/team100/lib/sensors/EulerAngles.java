c8e8aa90bf74b412eb549dc89247b3918e5607c4
Joel Truher <joel@truher.org>
2024-04-11 15:17:51 -0700

    minor cleanup of BNO086 code

--- lib/src/main/java/org/team100/lib/sensors/EulerAngles.java
+++ lib/src/main/java/org/team100/lib/sensors/EulerAngles.java
@@ -1,45 +1,21 @@
 package org.team100.lib.sensors;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+
 public class EulerAngles {
-    private double roll;
-    private double pitch;
-    private double yaw;
-    private double accuracy;
+    private final Rotation3d m_rotation;
+    private final double m_accuracy;
 
-    public EulerAngles() {
+    public EulerAngles(double roll, double pitch, double yaw, double accuracy) {
+        m_rotation = new Rotation3d(roll, pitch, yaw);
+        m_accuracy = accuracy;
     }
 
-    public double getRoll() {
-        return this.roll;
+    public Rotation3d getRotation() {
+        return m_rotation;
     }
 
-    public void setRoll(double roll) {
-        this.roll = roll;
-    }
-
-    public double getPitch() {
-        return this.pitch;
-    }
-
-    public void setPitch(double pitch) {
-        this.pitch = pitch;
-    }
-
-    public double getYaw() {
-        return this.yaw;
-    }
-
-    public void setYaw(double yaw) {
-        this.yaw = yaw;
-    }
-
-    public double getAcc() {
-        return this.accuracy;
-    }
-
-    public void setAcc(double acc) {
-        this.accuracy = acc;
+    public double getAccuracy() {
+        return m_accuracy;
     }
 }
-
-
