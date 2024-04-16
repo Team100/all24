7f9829fa822e0cd64e2265e21b89db155b8dd84d
Joel Truher <joel@truher.org>
2024-04-15 18:55:47 -0700

    fix the build

--- lib/src/main/java/org/team100/lib/localization/TimeInterpolatableBuffer100.java
+++ lib/src/main/java/org/team100/lib/localization/TimeInterpolatableBuffer100.java
@@ -118,23 +118,25 @@ public final class TimeInterpolatableBuffer100<T extends Interpolatable<T>> {
     }
 
     /**
-     * Return the floorEntry before t, and another floorEntry dt before the first.
+     * Return the lowerEntry before t. and another floorEntry dt before that.
+     * 
+     * The first is used as the basis for integration. The second is used to
+     * estimate velocity.
      * 
      * Writes are prohibited between the two reads, so that they are consistent.
-     * 
-     * This is used for velocity estimation.  We use "floorEntry" so that
-     * if there's an entry at exactly the right time, we use it; the alternative,
-     * "lowerEntry" would exclude an exact match.
      * 
      * This might return an empty list (if no entries exist before t) or one item
      * (if one entry exists before t, but there are no entries earlier than dt
      * before that), or two items.
+     * 
+     * If present, the first item in the list is the lowerEntry, and the second item
+     * is the earlierEntry, if present.
      */
     public List<Entry<Double, T>> consistentPair(double t, double dt) {
         try {
             // this pair should be consistent, so prohibit writes briefly.
             m_lock.writeLock().lock();
-            Entry<Double, T> lowerEntry = m_pastSnapshots.floorEntry(t);
+            Entry<Double, T> lowerEntry = m_pastSnapshots.lowerEntry(t);
             if (lowerEntry == null) {
                 // if there's no lower entry, then return nothing.
                 return List.of();
@@ -144,7 +146,8 @@ public final class TimeInterpolatableBuffer100<T extends Interpolatable<T>> {
                 // if there's no earlier entry, return the lower entry alone.
                 return List.of(lowerEntry);
             }
-            return List.of(earlierEntry, lowerEntry);
+
+            return List.of(lowerEntry, earlierEntry);
         } finally {
             m_lock.writeLock().unlock();
         }
