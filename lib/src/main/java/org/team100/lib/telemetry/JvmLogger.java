d2b3dd3517ed6b56f7c808811910931ae3a76287
Joel Truher <joel@truher.org>
2024-04-29 12:08:35 -0700

    add JVM logging for memory, GC events

---
+++ lib/src/main/java/org/team100/lib/telemetry/JvmLogger.java
@@ -0,0 +1,74 @@
+package org.team100.lib.telemetry;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryUsage;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.team100.lib.dashboard.Glassy;
+import org.team100.lib.telemetry.Telemetry.Level;
+import org.team100.lib.util.Names;
+
+/**
+ * Logs stuff about the JVM. Inspired by Advantage Kit's
+ * LoggedRobot.GcStatsCollector().
+ */
+public class JvmLogger implements Glassy {
+    private final Telemetry t = Telemetry.get();
+    private final String m_name;
+    private final Map<String, Long> times;
+    private final Map<String, Long> counts;
+
+    public JvmLogger() {
+        m_name = Names.name(this);
+        times = new HashMap<>();
+        counts = new HashMap<>();
+    }
+
+    public void logGarbageCollectors() {
+        long accumTime = 0;
+        long accumCount = 0;
+        for (GarbageCollectorMXBean bean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            String pool = bean.getName();
+            times.computeIfAbsent(pool, x -> 0l);
+            counts.computeIfAbsent(pool, x -> 0l);
+            long collectionTime = bean.getCollectionTime();
+            long collectionCount = bean.getCollectionCount();
+            long thisTime = collectionTime - times.get(pool);
+            long thisCount = collectionCount - counts.get(pool);
+            times.put(pool, collectionTime);
+            counts.put(pool, collectionCount);
+            t.log(Level.DEBUG, m_name, "GCTimeMS/" + pool, thisTime);
+            t.log(Level.DEBUG, m_name, "GCCounts/" + pool, thisCount);
+            accumTime += thisTime;
+            accumCount += thisCount;
+        }
+        t.log(Level.DEBUG, m_name, "GCTimeMS/total", accumTime);
+        t.log(Level.DEBUG, m_name, "GCCounts/total", accumCount);
+    }
+
+    public void logMemoryPools() {
+        long accumUsage = 0;
+        for (MemoryPoolMXBean bean : ManagementFactory.getMemoryPoolMXBeans()) {
+            MemoryUsage usage = bean.getUsage();
+            accumUsage += usage.getUsed();
+            t.log(Level.DEBUG, m_name, "MemoryPool/" + bean.getName(), usage.getUsed());
+        }
+        t.log(Level.DEBUG, m_name, "MemoryPool/total", accumUsage);
+    }
+
+    public void logMemoryUsage() {
+        MemoryMXBean bean = ManagementFactory.getMemoryMXBean();
+        t.log(Level.DEBUG, m_name, "MemoryUsage/heap", bean.getHeapMemoryUsage().getUsed());
+        t.log(Level.DEBUG, m_name, "MemoryUsage/non-heap", bean.getNonHeapMemoryUsage().getUsed());
+    }
+
+    @Override
+    public String getGlassName() {
+        return "JVM Logger";
+    }
+
+}
