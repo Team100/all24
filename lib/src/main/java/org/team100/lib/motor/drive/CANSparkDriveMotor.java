1a0d69c85c24be3a571465db575cb2adac9a3b4e
Joel Truher <joel@truher.org>
2024-05-27 17:02:07 -0700

    extract superclasses for neo/vortex motors

---
+++ lib/src/main/java/org/team100/lib/motor/drive/CANSparkDriveMotor.java
@@ -0,0 +1,204 @@
+package org.team100.lib.motor.drive;
+
+import org.team100.lib.config.Feedforward100;
+import org.team100.lib.config.PIDConstants;
+import org.team100.lib.motor.Motor100;
+import org.team100.lib.motor.MotorPhase;
+import org.team100.lib.motor.Rev100;
+import org.team100.lib.telemetry.Telemetry;
+import org.team100.lib.telemetry.Telemetry.Level;
+import org.team100.lib.units.Distance100;
+import org.team100.lib.util.Names;
+
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.SparkPIDController.ArbFFUnits;
+
+/**
+ * Linear drive motor using REV Neo or Neo Vortex.
+ */
+public abstract class CANSparkDriveMotor implements Motor100<Distance100> {
+    private final Telemetry t = Telemetry.get();
+    private final String m_name;
+    private final double m_gearRatio;
+    private final double m_wheelDiameter;
+    private final Feedforward100 m_ff;
+    private final CANSparkBase m_motor;
+    private final RelativeEncoder m_encoder;
+    private final SparkPIDController m_pidController;
+
+    CANSparkDriveMotor(
+            String name,
+            CANSparkBase motor,
+            MotorPhase motorPhase,
+            int currentLimit,
+            double gearRatio,
+            double wheelDiameter,
+            Feedforward100 ff,
+            PIDConstants pid) {
+        m_name = Names.append(name, this);
+        m_gearRatio = gearRatio;
+        m_wheelDiameter = wheelDiameter;
+        m_ff = ff;
+
+        m_motor = motor;
+
+        Rev100.baseConfig(m_motor);
+        Rev100.motorConfig(m_motor, IdleMode.kBrake, motorPhase, 20);
+        Rev100.currentConfig(m_motor, currentLimit);
+        m_encoder = m_motor.getEncoder();
+        m_pidController = m_motor.getPIDController();
+        Rev100.pidConfig(m_pidController, pid);
+
+        t.log(Level.TRACE, m_name, "Device ID", m_motor.getDeviceId());
+        t.register(Level.TRACE, m_name, "P", pid.getP(), this::setP);
+        t.register(Level.TRACE, m_name, "I", pid.getI(), this::setI);
+        t.register(Level.TRACE, m_name, "D", pid.getD(), this::setD);
+        t.register(Level.TRACE, m_name, "IZone", pid.getIZone(), this::setIZone);
+    }
+
+    @Override
+    public void setDutyCycle(double output) {
+        m_motor.set(output);
+        t.log(Level.TRACE, m_name, "Output", output);
+        log();
+    }
+
+    @Override
+    public void stop() {
+        m_motor.stopMotor();
+    }
+
+    /**
+     * Using the supplied wheel diameter and gear ratio, set the motor velocity
+     * to the correct RPM given the desired linear speed in m/s.
+     * 
+     * Supports accel feedforward.
+     * 
+     * Note the implementation here is surely wrong, it needs to be calibrated.
+     */
+    @Override
+    public void setVelocity(double outputM_S, double accelM_S2) {
+        double wheelRev_S = outputM_S / (m_wheelDiameter * Math.PI);
+        double motorRev_S = wheelRev_S * m_gearRatio;
+        double motorRev_M = motorRev_S * 60;
+
+        double wheelRev_S2 = accelM_S2 / (m_wheelDiameter * Math.PI);
+        double motorRev_S2 = wheelRev_S2 * m_gearRatio;
+
+        double velocityFFVolts = m_ff.velocityFFVolts(motorRev_S);
+        double frictionFFVolts = m_ff.frictionFFVolts(m_encoder.getVelocity() / 60, motorRev_S);
+        double accelFFVolts = m_ff.accelFFVolts(motorRev_S2);
+        double kFF = frictionFFVolts + velocityFFVolts + accelFFVolts;
+
+        Rev100.warn(() -> m_pidController.setReference(motorRev_M, ControlType.kVelocity, 0, kFF, ArbFFUnits.kVoltage));
+
+        t.log(Level.TRACE, m_name, "friction feedforward volts", frictionFFVolts);
+        t.log(Level.TRACE, m_name, "velocity feedforward volts", velocityFFVolts);
+        t.log(Level.TRACE, m_name, "accel feedforward volts", accelFFVolts);
+        t.log(Level.TRACE, m_name, "desired speed (rev_s)", motorRev_S);
+        log();
+    }
+
+    @Override
+    public void setVelocity(double outputM_S, double accelM_S2, double torqueNm) {
+        double wheelRev_S = outputM_S / (m_wheelDiameter * Math.PI);
+        double motorRev_S = wheelRev_S * m_gearRatio;
+        double motorRev_M = motorRev_S * 60;
+
+        double wheelRev_S2 = accelM_S2 / (m_wheelDiameter * Math.PI);
+        double motorRev_S2 = wheelRev_S2 * m_gearRatio;
+
+        double velocityFFVolts = m_ff.velocityFFVolts(motorRev_S);
+        double frictionFFVolts = m_ff.frictionFFVolts(m_encoder.getVelocity() / 60, motorRev_S);
+        double accelFFVolts = m_ff.accelFFVolts(motorRev_S2);
+
+        double torqueFFAmps = torqueNm / kTNm_amp();
+        double torqueFFVolts = torqueFFAmps * kROhms();
+
+        double kFF = frictionFFVolts + velocityFFVolts + accelFFVolts + torqueFFVolts;
+
+        Rev100.warn(() -> m_pidController.setReference(motorRev_M, ControlType.kVelocity, 0, kFF, ArbFFUnits.kVoltage));
+
+        t.log(Level.TRACE, m_name, "friction feedforward volts", frictionFFVolts);
+        t.log(Level.TRACE, m_name, "velocity feedforward volts", velocityFFVolts);
+        t.log(Level.TRACE, m_name, "accel feedforward volts", accelFFVolts);
+        t.log(Level.TRACE, m_name, "torque feedforward volts", torqueFFVolts);
+        t.log(Level.TRACE, m_name, "desired speed (rev_s)", motorRev_S);
+        log();
+    }
+
+    @Override
+    public double getTorque() {
+        return m_motor.getOutputCurrent() * kTNm_amp();
+    }
+
+    @Override
+    public void close() {
+        m_motor.close();
+    }
+
+    /**
+     * @return integrated sensor position in rotations.
+     */
+    public double getPositionRot() {
+        // this is fast so we don't need to cache it
+        return m_encoder.getPosition();
+    }
+
+    /**
+     * @return integrated sensor velocity in RPM
+     */
+    public double getRateRPM() {
+        // this is fast so we don't need to cache it
+        return m_encoder.getVelocity();
+    }
+
+    /**
+     * Sets integrated sensor position to zero.
+     */
+    public void resetPosition() {
+        Rev100.warn(() -> m_encoder.setPosition(0));
+    }
+
+    public void log() {
+        t.log(Level.TRACE, m_name, "position (rev)", m_encoder.getPosition());
+        t.log(Level.TRACE, m_name, "velocity (rev_s)", m_encoder.getVelocity() / 60);
+        t.log(Level.TRACE, m_name, "current (A)", m_motor.getOutputCurrent());
+        t.log(Level.TRACE, m_name, "duty cycle", m_motor.getAppliedOutput());
+        t.log(Level.TRACE, m_name, "temperature (C)", m_motor.getMotorTemperature());
+    }
+
+    /**
+     * Motor resistance in ohms, used to calculate voltage from desired torque
+     * current.
+     */
+    abstract double kROhms();
+
+    /**
+     * Motor torque constant, kT, in Nm per amp, used to calculate current from
+     * desired torque.
+     */
+    abstract double kTNm_amp();
+
+    /////////////////////////////////////////////////////////////////
+
+    private void setP(double p) {
+        Rev100.warn(() -> m_pidController.setP(p));
+    }
+
+    private void setI(double i) {
+        Rev100.warn(() -> m_pidController.setI(i));
+    }
+
+    private void setD(double d) {
+        Rev100.warn(() -> m_pidController.setD(d));
+    }
+
+    private void setIZone(double iz) {
+        Rev100.warn(() -> m_pidController.setIZone(iz));
+    }
+}
--- lib/src/main/java/org/team100/lib/motor/turning/NeoVortexTurningMotor.java
+++ lib/src/main/java/org/team100/lib/motor/turning/NeoVortexTurningMotor.java
@@ -2,197 +2,37 @@ package org.team100.lib.motor.turning;
 
 import org.team100.lib.config.Feedforward100;
 import org.team100.lib.config.PIDConstants;
-import org.team100.lib.motor.Motor100;
 import org.team100.lib.motor.MotorPhase;
-import org.team100.lib.motor.Rev100;
-import org.team100.lib.telemetry.Telemetry;
-import org.team100.lib.telemetry.Telemetry.Level;
-import org.team100.lib.units.Angle100;
-import org.team100.lib.util.Names;
 
-import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkPIDController;
-import com.revrobotics.SparkPIDController.ArbFFUnits;
 
 /**
- * Swerve steering motor using REV Neo.
- * 
- * This is not finished, don't use it without finishing it.
+ * Swerve steering motor using REV Neo Vortex.
  */
-public class NeoVortexTurningMotor implements Motor100<Angle100> {
-    /**
-     * Motor resistance https://www.revrobotics.com/rev-21-1652/
-     */
-    private static final double kROhms = 0.057;
-    /**
-     * Motor torque constant https://www.revrobotics.com/rev-21-1652/
-     */
-    private static final double kTNm_amp = 0.017;
+public class NeoVortexTurningMotor extends CANSparkTurningMotor {
 
-    private final RelativeEncoder m_encoder;
-
-    /**
-     * This is surely wrong.
-     */
-    private final double m_gearRatio;
-
-    private final Feedforward100 m_ff;
-
-    /**
-     * Proportional feedback coefficient for the controller.
-     * 
-     * This is a guess. Calibrate it before using it.
-     */
-
-    private final Telemetry t = Telemetry.get();
-    private final SparkPIDController m_pidController;
-    private final CANSparkFlex m_motor;
-    private final String m_name;
-
-    public NeoVortexTurningMotor(String name,
+    public NeoVortexTurningMotor(
+            String name,
             int canId,
             MotorPhase motorPhase,
             int currentLimit,
             double gearRatio,
             Feedforward100 ff,
             PIDConstants pid) {
-        m_name = Names.append(name, this);
-        m_gearRatio = gearRatio;
-        m_ff = ff;
-
-        m_motor = new CANSparkFlex(canId, MotorType.kBrushless);
-        Rev100.baseConfig(m_motor);
-        Rev100.motorConfig(m_motor, IdleMode.kBrake, motorPhase, 20);
-        Rev100.currentConfig(m_motor, currentLimit);
-        m_encoder = m_motor.getEncoder();
-        m_pidController = m_motor.getPIDController();
-        Rev100.pidConfig(m_pidController, pid);
-
-        t.log(Level.TRACE, m_name, "Device ID", m_motor.getDeviceId());
-        t.register(Level.TRACE, m_name, "P", pid.getP(), this::setP);
-        t.register(Level.TRACE, m_name, "I", pid.getI(), this::setI);
-        t.register(Level.TRACE, m_name, "D", pid.getD(), this::setD);
-        t.register(Level.TRACE, m_name, "IZone", pid.getIZone(), this::setIZone);
+        super(name, new CANSparkFlex(canId, MotorType.kBrushless),
+                motorPhase, currentLimit, gearRatio, ff, pid);
     }
 
     @Override
-    public void setDutyCycle(double output) {
-        m_motor.set(output);
-        t.log(Level.TRACE, m_name, "desired duty cycle [-1,1]", output);
-        log();
+    double kROhms() {
+        // @see https://www.revrobotics.com/rev-21-1652/
+        return 0.057;
     }
 
     @Override
-    public void stop() {
-        m_motor.stopMotor();
-    }
-
-    /**
-     * Supports accel feedforward.
-     * 
-     * Note the implementation here is surely wrong, it needs to be calibrated.
-     */
-    @Override
-    public void setVelocity(double outputRad_S, double accelRad_S2) {
-        double motorRad_S = m_gearRatio * outputRad_S;
-        double motorRevs_S = motorRad_S / (2 * Math.PI);
-        double motorRevs_M = motorRevs_S * 60;
-        double motorRad_S2 = m_gearRatio * accelRad_S2;
-        double motorRevs_S2 = motorRad_S2 / (2 * Math.PI);
-
-        double velocityFFVolts = m_ff.velocityFFVolts(motorRevs_S);
-        double frictionFFVolts = m_ff.frictionFFVolts(m_encoder.getVelocity() / 60, motorRevs_S);
-        double accelFFVolts = m_ff.accelFFVolts(motorRevs_S2);
-
-        double kFF = frictionFFVolts + velocityFFVolts + accelFFVolts;
-
-        Rev100.warn(
-                () -> m_pidController.setReference(motorRevs_M, ControlType.kVelocity, 0, kFF, ArbFFUnits.kVoltage));
-
-        t.log(Level.TRACE, m_name, "friction feedforward [-1,1]", frictionFFVolts);
-        t.log(Level.TRACE, m_name, "velocity feedforward [-1,1]", velocityFFVolts);
-        t.log(Level.TRACE, m_name, "accel feedforward [-1,1]", accelFFVolts);
-        t.log(Level.TRACE, m_name, "desired speed (rev_s)", motorRevs_S);
-        log();
-    }
-
-    @Override
-    public void setVelocity(double outputRad_S, double accelRad_S2, double torqueNm) {
-        double motorRad_S = m_gearRatio * outputRad_S;
-        double motorRevs_S = motorRad_S / (2 * Math.PI);
-        double motorRevs_M = motorRevs_S * 60;
-        double motorRad_S2 = m_gearRatio * accelRad_S2;
-        double motorRevs_S2 = motorRad_S2 / (2 * Math.PI);
-
-        double velocityFFVolts = m_ff.velocityFFVolts(motorRevs_S);
-        double frictionFFVolts = m_ff.frictionFFVolts(m_encoder.getVelocity() / 60, motorRevs_S);
-        double accelFFVolts = m_ff.accelFFVolts(motorRevs_S2);
-
-        double torqueFFAmps = torqueNm / kTNm_amp;
-        double torqueFFVolts = torqueFFAmps * kROhms;
-
-        double kFF = frictionFFVolts + velocityFFVolts + accelFFVolts + torqueFFVolts;
-
-        Rev100.warn(
-                () -> m_pidController.setReference(motorRevs_M, ControlType.kVelocity, 0, kFF, ArbFFUnits.kVoltage));
-
-        t.log(Level.TRACE, m_name, "friction feedforward volts", frictionFFVolts);
-        t.log(Level.TRACE, m_name, "velocity feedforward volts", velocityFFVolts);
-        t.log(Level.TRACE, m_name, "accel feedforward volts", accelFFVolts);
-        t.log(Level.TRACE, m_name, "torque feedforward volts", torqueFFVolts);
-        t.log(Level.TRACE, m_name, "desired speed (rev_s)", motorRevs_S);
-        log();
-    }
-
-    @Override
-    public double getTorque() {
-        return m_motor.getOutputCurrent() * kTNm_amp;
-    }
-
-    public void resetPosition() {
-        Rev100.warn(() -> m_encoder.setPosition(0));
-    }
-
-    @Override
-    public void close() {
-        m_motor.close();
-    }
-
-    public double getPositionRot() {
-        return m_encoder.getPosition();
-    }
-
-    public double getRateRPM() {
-        return m_encoder.getVelocity();
-    }
-
-    public void log() {
-        t.log(Level.TRACE, m_name, "position (rev)", m_encoder.getPosition());
-        t.log(Level.TRACE, m_name, "velocity (rev_s)", m_encoder.getVelocity() / 60);
-        t.log(Level.TRACE, m_name, "current (A)", m_motor.getOutputCurrent());
-        t.log(Level.TRACE, m_name, "duty cycle", m_motor.getAppliedOutput());
-        t.log(Level.TRACE, m_name, "temperature (C)", m_motor.getMotorTemperature());
-    }
-
-    /////////////////////////////////////////////////////////////////
-
-    private void setP(double p) {
-        Rev100.warn(() -> m_pidController.setP(p));
-    }
-
-    private void setI(double i) {
-        Rev100.warn(() -> m_pidController.setI(i));
-    }
-
-    private void setD(double d) {
-        Rev100.warn(() -> m_pidController.setD(d));
-    }
-
-    private void setIZone(double iz) {
-        Rev100.warn(() -> m_pidController.setIZone(iz));
+    double kTNm_amp() {
+        // @see https://www.revrobotics.com/rev-21-1652/
+        return 0.017;
     }
 }
