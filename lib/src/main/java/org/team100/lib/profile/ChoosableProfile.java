137f0251f342e7fc604d8106eb0bfa81c766a720
Joel Truher <joel@truher.org>
2024-06-15 11:22:19 -0700

    remove exponential profile, we probably just don't need it.

--- lib/src/main/java/org/team100/lib/profile/ChoosableProfile.java
+++ lib/src/main/java/org/team100/lib/profile/ChoosableProfile.java
@@ -3,7 +3,6 @@ package org.team100.lib.profile;
 import org.team100.lib.controller.State100;
 import org.team100.lib.telemetry.ProfileModeChooser;
 
-import edu.wpi.first.math.trajectory.ExponentialProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -15,12 +14,7 @@ public class ChoosableProfile implements Profile100 {
         /** The WPILib trapezoidal profile with infinite jerk */
         PROFILE_WPI,
         /** The Team 100 profile, which handles some cases the WPI one does not. */
-        PROFILE_100,
-        /**
-         * New for 2024, takes motor EMF into account.
-         * This should be considered experimental.
-         */
-        EXPONENTIAL
+        PROFILE_100
     }
 
     private final SendableChooser<Mode> m_chooser;
@@ -30,7 +24,6 @@ public class ChoosableProfile implements Profile100 {
     // the profile object once.
     private final ProfileWPI m_profileWPI;
     private final TrapezoidProfile100 m_trapezoid;
-    private final ExponentialProfile eprofile;
 
     /** The default is specifiable mostly for testing. */
     public ChoosableProfile(
@@ -48,13 +41,6 @@ public class ChoosableProfile implements Profile100 {
 
         m_trapezoid = new TrapezoidProfile100(
                 new Constraints100(maxVel, maxAccel), 0.05);
-
-        // These constraints are completely made up.
-        // If you want to try the exponential profile, you'll have to
-        // calibrate them
-        eprofile = new ExponentialProfile(
-                ExponentialProfile.Constraints.fromCharacteristics(
-                        10, 10, 5));
     }
 
     /** Note order here, initial first, goal second. */
@@ -65,11 +51,6 @@ public class ChoosableProfile implements Profile100 {
                 return m_profileWPI.calculate(dt, initial, goal);
             case PROFILE_100:
                 return m_trapezoid.calculate(dt, initial, goal);
-            case EXPONENTIAL:
-                ExponentialProfile.State estate = eprofile.calculate(dt,
-                        new ExponentialProfile.State(initial.x(), initial.v()),
-                        new ExponentialProfile.State(goal.x(), goal.v()));
-                return new State100(estate.position, estate.velocity);
             default:
                 return new State100(0, 0);
         }
