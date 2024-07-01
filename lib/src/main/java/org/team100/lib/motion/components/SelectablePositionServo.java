56f4fb326447ef2feffbeaef675d5d7653ea8b6b
truher <joel@truher.org>
2024-07-01 13:43:17 -0700

    make onboard/outboard position servo selectable at runtime

---
+++ lib/src/main/java/org/team100/lib/motion/components/SelectablePositionServo.java
@@ -0,0 +1,119 @@
+package org.team100.lib.motion.components;
+
+import java.util.OptionalDouble;
+import java.util.function.BooleanSupplier;
+
+import org.team100.lib.controller.State100;
+import org.team100.lib.units.Measure100;
+
+/**
+ * Select one of two position servo implementations.
+ * 
+ * This allows the servo to be selected at runtime.
+ */
+public class SelectablePositionServo<T extends Measure100> implements PositionServo<T> {
+
+    private final PositionServo<T> m_whenTrue;
+    private final PositionServo<T> m_whenFalse;
+    private final BooleanSupplier m_selector;
+
+    public SelectablePositionServo(
+            PositionServo<T> whenTrue,
+            PositionServo<T> whenFalse,
+            BooleanSupplier selector) {
+        m_whenTrue = whenTrue;
+        m_whenFalse = whenFalse;
+        m_selector = selector;
+    }
+
+    @Override
+    public void reset() {
+        if (m_selector.getAsBoolean()) {
+            m_whenTrue.reset();
+        } else {
+            m_whenFalse.reset();
+        }
+    }
+
+    @Override
+    public void setPosition(double goal, double feedForwardTorqueNm) {
+        if (m_selector.getAsBoolean()) {
+            m_whenTrue.setPosition(goal, feedForwardTorqueNm);
+        } else {
+            m_whenFalse.setPosition(goal, feedForwardTorqueNm);
+        }
+    }
+
+    @Override
+    public OptionalDouble getPosition() {
+        if (m_selector.getAsBoolean()) {
+            return m_whenTrue.getPosition();
+        } else {
+            return m_whenFalse.getPosition();
+        }
+    }
+
+    @Override
+    public OptionalDouble getVelocity() {
+        if (m_selector.getAsBoolean()) {
+            return m_whenTrue.getVelocity();
+        } else {
+            return m_whenFalse.getVelocity();
+        }
+    }
+
+    @Override
+    public boolean atSetpoint() {
+        if (m_selector.getAsBoolean()) {
+            return m_whenTrue.atSetpoint();
+        } else {
+            return m_whenFalse.atSetpoint();
+        }
+    }
+
+    @Override
+    public boolean atGoal() {
+        if (m_selector.getAsBoolean()) {
+            return m_whenTrue.atGoal();
+        } else {
+            return m_whenFalse.atGoal();
+        }
+    }
+
+    @Override
+    public double getGoal() {
+        if (m_selector.getAsBoolean()) {
+            return m_whenTrue.getGoal();
+        } else {
+            return m_whenFalse.getGoal();
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (m_selector.getAsBoolean()) {
+            m_whenTrue.stop();
+        } else {
+            m_whenFalse.stop();
+        }
+    }
+
+    @Override
+    public void close() {
+        if (m_selector.getAsBoolean()) {
+            m_whenTrue.close();
+        } else {
+            m_whenFalse.close();
+        }
+    }
+
+    @Override
+    public State100 getSetpoint() {
+        if (m_selector.getAsBoolean()) {
+            return m_whenTrue.getSetpoint();
+        } else {
+            return m_whenFalse.getSetpoint();
+        }
+    }
+
+}
