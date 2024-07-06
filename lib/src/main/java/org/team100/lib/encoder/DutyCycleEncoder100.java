e18f5673fd0b079930cdb7ddfe95996a9dbefc60
Joel Truher <joel@truher.org>
2024-07-06 10:27:05 -0700

    update duty cycle encoder

--- lib/src/main/java/org/team100/lib/encoder/DutyCycleEncoder100.java
+++ lib/src/main/java/org/team100/lib/encoder/DutyCycleEncoder100.java
@@ -7,15 +7,32 @@ import org.team100.lib.telemetry.Telemetry.Level;
 import org.team100.lib.units.Distance100;
 import org.team100.lib.util.Util;
 
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Duty cycle encoder such as the AMS 5048.
+ * 
+ * The 2025 update changed the DutyCycleEncoder class a lot, so i copied some of
+ * the old methods here.
+ * 
+ * TODO: this should be further cleaned up once the 2025 transition work is
+ * done.
+ * 
+ * TODO: combine with DutyCycleTurningEncoder
  */
 public class DutyCycleEncoder100 implements Encoder100<Distance100> {
     private final Logger m_logger;
-    public final DutyCycleEncoder m_encoder;
+    private final DigitalInput m_digitalInput;
+    private final DutyCycle m_dutyCycle;
+    private final int m_frequencyThreshold;
+    private final double m_positionOffset;
+    private final double m_distancePerRotation;
+    // TODO: use these to fix https://github.com/Team100/all24/issues/383
+    // these are within [0,1]
+    private double m_sensorMin = 0.0;
+    private double m_sensorMax = 1.0;
 
     private Double prevAngle = null;
     private Double prevTime = null;
@@ -35,24 +52,25 @@ public class DutyCycleEncoder100 implements Encoder100<Distance100> {
             boolean reversed) {
         m_reversed = reversed;
         m_logger = parent.child(this);
-        m_encoder = new DutyCycleEncoder(channel);
-        m_encoder.setPositionOffset(inputOffset);
-        m_encoder.setDistancePerRotation(2 * Math.PI);
-        m_encoder.setConnectedFrequencyThreshold(1000);
+        m_digitalInput = new DigitalInput(channel);
+        m_dutyCycle = new DutyCycle(m_digitalInput);
+        m_distancePerRotation = 2 * Math.PI;
+        m_positionOffset = inputOffset;
+        m_frequencyThreshold = 1000;
     }
 
     @Override
     public OptionalDouble getPosition() {
-        if (!m_encoder.isConnected()) {
-            Util.warn(String.format("encoder %d not connected", m_encoder.getSourceChannel()));
+        if (!isConnected()) {
+            Util.warn(String.format("encoder %d not connected", m_dutyCycle.getSourceChannel()));
             return OptionalDouble.empty();
         }
         if (m_reversed) {
-            return OptionalDouble.of(-1.0 * (m_encoder.getAbsolutePosition() - m_encoder.getPositionOffset())
-                    * m_encoder.getDistancePerRotation());
+            return OptionalDouble.of(-1.0 * (getAbsolutePosition() - m_positionOffset)
+                    * m_distancePerRotation);
         }
         return OptionalDouble.of(
-                (m_encoder.getAbsolutePosition() - m_encoder.getPositionOffset()) * m_encoder.getDistancePerRotation());
+                (getAbsolutePosition() - m_positionOffset) * m_distancePerRotation);
     }
 
     /**
@@ -68,8 +86,8 @@ public class DutyCycleEncoder100 implements Encoder100<Distance100> {
      */
     @Override
     public OptionalDouble getRate() {
-        if (!m_encoder.isConnected()) {
-            Util.warn(String.format("encoder %d not connected", m_encoder.getSourceChannel()));
+        if (!isConnected()) {
+            Util.warn(String.format("encoder %d not connected", m_dutyCycle.getSourceChannel()));
             return OptionalDouble.empty();
         }
         return OptionalDouble.of(getRateRad_S());
@@ -84,18 +102,55 @@ public class DutyCycleEncoder100 implements Encoder100<Distance100> {
     }
 
     public void close() {
-        // m_input.close();
-        m_encoder.close();
+        m_dutyCycle.close();
+        m_digitalInput.close();
     }
 
     //////////////////////////////////////////////
 
+    private boolean isConnected() {
+        return m_dutyCycle.getFrequency() > m_frequencyThreshold;
+    }
+
+    /** map to full [0,1] */
+    private double mapSensorRange(double pos) {
+        // map sensor range
+        if (pos < m_sensorMin) {
+            pos = m_sensorMin;
+        }
+        if (pos > m_sensorMax) {
+            pos = m_sensorMax;
+        }
+        pos = (pos - m_sensorMin) / (m_sensorMax - m_sensorMin);
+        return pos;
+    }
+
+    /** return turns [0,1] */
+    private double getAbsolutePosition() {
+        return mapSensorRange(m_dutyCycle.getOutput());
+    }
+
+    /**
+     * return turns minus offset, can be outside [0,1]
+     * TODO: modulus to [0,1]
+     */
+    private double get() {
+        double dutyCycle = m_dutyCycle.getOutput();
+        double posTurns = mapSensorRange(dutyCycle);
+        return posTurns - m_positionOffset;
+    }
+
+    private double getDistance() {
+        return get() * m_distancePerRotation;
+    }
+
     private double getPositionRad() {
-        m_logger.logDouble(Level.TRACE, "position (rad)", m_encoder::getDistance);
-        m_logger.logDouble(Level.TRACE, "position (turns)", m_encoder::get);
-        m_logger.logDouble(Level.TRACE, "position (absolute)", m_encoder::getAbsolutePosition);
         // should be fast, no cache needed.
-        return m_encoder.getDistance();
+        double posRad = getDistance();
+        m_logger.logDouble(Level.TRACE, "position (rad)", () -> posRad);
+        m_logger.logDouble(Level.TRACE, "position (turns-offset)", this::get);
+        m_logger.logDouble(Level.TRACE, "position (absolute)", this::getAbsolutePosition);
+        return posRad;
     }
 
     /** finite difference == noise! */
--- lib/src/main/java/org/team100/lib/encoder/turning/DutyCycleTurningEncoder.java
+++ lib/src/main/java/org/team100/lib/encoder/turning/DutyCycleTurningEncoder.java
@@ -9,7 +9,8 @@ import org.team100.lib.units.Angle100;
 import org.team100.lib.util.Util;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
@@ -17,10 +18,28 @@ import edu.wpi.first.wpilibj.Timer;
  * 
  * This is a near-copy of AnalogTurningEncoder; sadly the underlying WPI types
  * have no common parent, so there's some duplication here.
+ * 
+ * The 2025 update changed the DutyCycleEncoder class a lot, so i copied some of
+ * the old methods here.
+ * 
+ * TODO: this should be further cleaned up once the 2025 transition work is
+ * done.
+ * 
+ * TODO: combine with DutyCycleEncoder100.
+ * 
+ * TODO: remove gear ratio, it's always 1.
  */
 public class DutyCycleTurningEncoder implements Encoder100<Angle100> {
     private final Logger m_logger;
-    private final DutyCycleEncoder m_encoder;
+    private final DigitalInput m_digitalInput;
+    private final DutyCycle m_dutyCycle;
+    private final int m_frequencyThreshold;
+    private final double m_positionOffset;
+    private final double m_distancePerRotation;
+    // TODO: use these to fix https://github.com/Team100/all24/issues/383
+    // these are within [0,1]
+    private double m_sensorMin = 0.0;
+    private double m_sensorMax = 1.0;
 
     private Double prevAngle = null;
     private Double prevTime = null;
@@ -32,24 +51,27 @@ public class DutyCycleTurningEncoder implements Encoder100<Angle100> {
             double gearRatio,
             EncoderDrive drive) {
         m_logger = parent.child(this);
-        m_encoder = new DutyCycleEncoder(channel);
-        m_encoder.setPositionOffset(inputOffset);
+        m_digitalInput = new DigitalInput(channel);
+        m_dutyCycle = new DutyCycle(m_digitalInput);
+        m_positionOffset = inputOffset;
         switch (drive) {
             case DIRECT:
-                m_encoder.setDistancePerRotation(2.0 * Math.PI / gearRatio);
+                m_distancePerRotation = 2.0 * Math.PI / gearRatio;
                 break;
             case INVERSE:
-                m_encoder.setDistancePerRotation(2.0 * Math.PI / (-1.0 * gearRatio));
+                m_distancePerRotation = 2.0 * Math.PI / (-1.0 * gearRatio);
                 break;
+            default:
+                throw new IllegalArgumentException();
         }
-
-        m_logger.logInt(Level.TRACE, "channel", m_encoder::getSourceChannel);
+        m_frequencyThreshold = 1000;
+        m_logger.logInt(Level.TRACE, "channel", m_dutyCycle::getSourceChannel);
     }
 
     @Override
     public OptionalDouble getPosition() {
-        if (!m_encoder.isConnected()) {
-            Util.warn(String.format("encoder %d not connected", m_encoder.getSourceChannel()));
+        if (!isConnected()) {
+            Util.warn(String.format("encoder %d not connected", m_dutyCycle.getSourceChannel()));
             return OptionalDouble.empty();
         }
         return OptionalDouble.of(getPositionRad());
@@ -57,8 +79,8 @@ public class DutyCycleTurningEncoder implements Encoder100<Angle100> {
 
     @Override
     public OptionalDouble getRate() {
-        if (!m_encoder.isConnected()) {
-            Util.warn(String.format("encoder %d not connected", m_encoder.getSourceChannel()));
+        if (!isConnected()) {
+            Util.warn(String.format("encoder %d not connected", m_dutyCycle.getSourceChannel()));
             return OptionalDouble.empty();
         }
         return OptionalDouble.of(getRateRad_S());
@@ -74,16 +96,53 @@ public class DutyCycleTurningEncoder implements Encoder100<Angle100> {
 
     @Override
     public void close() {
-        m_encoder.close();
+        m_dutyCycle.close();
+        m_digitalInput.close();
     }
 
     ////////////////////////////////////
 
+    private boolean isConnected() {
+        return m_dutyCycle.getFrequency() > m_frequencyThreshold;
+    }
+
+    /** map to full [0,1] */
+    private double mapSensorRange(double pos) {
+        // map sensor range
+        if (pos < m_sensorMin) {
+            pos = m_sensorMin;
+        }
+        if (pos > m_sensorMax) {
+            pos = m_sensorMax;
+        }
+        pos = (pos - m_sensorMin) / (m_sensorMax - m_sensorMin);
+        return pos;
+    }
+
+    /** return turns [0,1] */
+    private double getAbsolutePosition() {
+        return mapSensorRange(m_dutyCycle.getOutput());
+    }
+
+    /**
+     * return turns minus offset, can be outside [0,1]
+     * TODO: modulus to [0,1]
+     */
+    private double get() {
+        double dutyCycle = m_dutyCycle.getOutput();
+        double posTurns = mapSensorRange(dutyCycle);
+        return posTurns - m_positionOffset;
+    }
+
+    private double getDistance() {
+        return get() * m_distancePerRotation;
+    }
+
     private double getPositionRad() {
-        double positionRad = m_encoder.getDistance();
+        double positionRad = getDistance();
         m_logger.logDouble(Level.TRACE, "position (rad) ROBOT USES THIS (CCW POSITIVE)", () -> positionRad);
-        m_logger.logDouble(Level.TRACE, "position (turns) USE FOR OFFSETS", m_encoder::get);
-        m_logger.logDouble(Level.TRACE, "position (absolute)", m_encoder::getAbsolutePosition);
+        m_logger.logDouble(Level.TRACE, "position (turns-offset) USE FOR OFFSETS", this::get);
+        m_logger.logDouble(Level.TRACE, "position (absolute)", this::getAbsolutePosition);
         m_logger.logDouble(Level.TRACE, "Wrapped position rads (absolute)", () -> MathUtil.angleModulus(positionRad));
         return positionRad;
     }
