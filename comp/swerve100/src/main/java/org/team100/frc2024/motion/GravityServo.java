678349c1788d1289f2838e60a8872818b1313137
Joel Truher <joel@truher.org>
2024-07-18 11:08:03 -0700

    poke at gravity servo a bit; needs work

--- comp/swerve100/src/main/java/org/team100/frc2024/motion/GravityServo.java
+++ comp/swerve100/src/main/java/org/team100/frc2024/motion/GravityServo.java
@@ -9,6 +9,7 @@ import org.team100.lib.motion.RotaryMechanism;
 import org.team100.lib.profile.Profile100;
 import org.team100.lib.telemetry.SupplierLogger;
 import org.team100.lib.telemetry.Telemetry.Level;
+import org.team100.lib.util.Util;
 
 import edu.wpi.first.math.controller.PIDController;
 
@@ -26,8 +27,7 @@ public class GravityServo implements Glassy {
     private final double m_period;
     private final RotaryPositionSensor m_encoder;
 
-    private State100 m_goal = new State100(0, 0);
-    private State100 m_setpoint = new State100(0, 0);
+    private State100 m_setpointRad = new State100(0, 0);
 
     public GravityServo(
             RotaryMechanism motor,
@@ -48,37 +48,36 @@ public class GravityServo implements Glassy {
     /** Zeros controller errors, sets setpoint to current position. */
     public void reset() {
         m_controller.reset();
-        if (getPositionRad().isEmpty()) {
+        OptionalDouble opt = getPositionRad();
+        if (opt.isEmpty()) {
+            Util.warn("GravityServo: Broken sensor!");
             return;
         }
-        m_setpoint = new State100(getPositionRad().getAsDouble(), 0);
+        m_setpointRad = new State100(opt.getAsDouble(), 0);
     }
 
     public OptionalDouble getPositionRad() {
         return m_encoder.getPositionRad();
     }
 
-    public void setPosition(double goal) {
-        OptionalDouble encoderPosition = m_encoder.getPositionRad();
-        if (encoderPosition.isEmpty()) {
+    public void setPosition(double goalRad) {
+        OptionalDouble opt = getPositionRad();
+        if (opt.isEmpty()) {
+            Util.warn("GravityServo: Broken sensor!");
             return;
         }
-        double mechanismPosition = encoderPosition.getAsDouble();
+        double mechanismPositionRad = opt.getAsDouble();
 
         // note zero velocity in the goal.
-        m_goal = new State100(goal, 0.0);
+        State100 m_goal = new State100(goalRad, 0.0);
 
-        m_setpoint = new State100(
-                (m_setpoint.x()),
-                m_setpoint.v());
+        m_setpointRad = m_profile.calculate(m_period, m_setpointRad, m_goal);
 
-        m_setpoint = m_profile.calculate(m_period, m_setpoint, m_goal);
+        final double u_FB = m_controller.calculate(mechanismPositionRad, m_setpointRad.x());
+        final double u_FF = m_setpointRad.v() * 0.5;
 
-        final double u_FB = m_controller.calculate(mechanismPosition, m_setpoint.x());
-        final double u_FF = m_setpoint.v() * 0.5; // rot/s to rpm conversion
-
-        final double gravityTorque = 0.006 * Math.cos(mechanismPosition);
-        final double staticFF = getStaticFF(mechanismPosition, u_FB, u_FF);
+        final double gravityTorque = 0.006 * Math.cos(mechanismPositionRad);
+        final double staticFF = getStaticFF(mechanismPositionRad, goalRad, u_FB, u_FF);
 
         // final double u_TOTAL = gravityTorque + u_FF + u_FB + staticFF;
         final double u_TOTAL = gravityTorque + u_FF + u_FB;
@@ -91,33 +90,30 @@ public class GravityServo implements Glassy {
         m_logger.logDouble(Level.TRACE, "static FF", () -> staticFF);
         m_logger.logDouble(Level.TRACE, "gravity T", () -> gravityTorque);
         m_logger.logDouble(Level.TRACE, "u_TOTAL", () -> u_TOTAL);
-        m_logger.logDouble(Level.TRACE, "Measurement", () -> mechanismPosition);
-        m_logger.logState100(Level.TRACE, "Goal", () -> m_goal);
-        m_logger.logState100(Level.TRACE, "Setpoint", () -> m_setpoint);
-        m_logger.logDouble(Level.TRACE, "Setpoint Velocity", m_setpoint::v);
-        m_logger.logDouble(Level.TRACE, "Controller Position Error", m_controller::getPositionError);
-        m_logger.logDouble(Level.TRACE, "Controller Velocity Error", m_controller::getVelocityError);
-    }
-
-    private double getStaticFF(double measurement, double u_FB, double u_FF) {
-        double staticFF = 0.01 * Math.signum(u_FF + u_FB);
-
-        if (Math.abs(m_goal.x() - measurement) < m_controller.getPositionTolerance()) {
-            staticFF = 0;
-        }
-        return staticFF;
-    }
-
-    public void set(double value) {
-        m_motor.setDutyCycle(value);
+        m_logger.logDouble(Level.TRACE, "Measurement (rad)", () -> mechanismPositionRad);
+        m_logger.logState100(Level.TRACE, "Goal (rad)", () -> m_goal);
+        m_logger.logState100(Level.TRACE, "Setpoint (rad)", () -> m_setpointRad);
+        m_logger.logDouble(Level.TRACE, "Setpoint Velocity", m_setpointRad::v);
+        m_logger.logDouble(Level.TRACE, "Controller Position Error (rad)", m_controller::getPositionError);
+        m_logger.logDouble(Level.TRACE, "Controller Velocity Error (rad_s)", m_controller::getVelocityError);
     }
 
     public void stop() {
-        m_motor.setDutyCycle(0);
+        m_motor.stop();
     }
 
     @Override
     public String getGlassName() {
         return "GravityServo";
     }
+
+    private double getStaticFF(double measurementRad, double goalRad, double u_FB, double u_FF) {
+        double staticFF = 0.01 * Math.signum(u_FF + u_FB);
+
+        if (Math.abs(goalRad - measurementRad) < m_controller.getPositionTolerance()) {
+            staticFF = 0;
+        }
+        return staticFF;
+    }
+
 }
