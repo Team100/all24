e3f3cf9d4d4f5ed0906aec21352403652a275612
Joel Truher <joel@truher.org>
2024-07-18 20:19:22 -0700

    arm tuning

--- comp/swerve100/src/main/java/org/team100/frc2024/motion/amp/AmpPivot.java
+++ comp/swerve100/src/main/java/org/team100/frc2024/motion/amp/AmpPivot.java
@@ -24,6 +24,7 @@ import org.team100.lib.motor.SimulatedBareMotor;
 import org.team100.lib.profile.Dashpot;
 import org.team100.lib.profile.JerkLimiter;
 import org.team100.lib.profile.Profile100;
+import org.team100.lib.profile.SelectableProfile100;
 import org.team100.lib.profile.TrapezoidProfile100;
 import org.team100.lib.telemetry.SupplierLogger;
 
@@ -34,14 +35,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * The pivot is independent from the feeder, so it's a separate subsystem.
  */
 public class AmpPivot extends SubsystemBase implements Glassy {
+
+    /**
+     * TODO: units?
+     */
+    // TODO: i've abandoned outboard velocity pid for position control
+    // instead use outboard position control and a combined encoder.
+    private static final double kOutboardP = 0.0;// 0.0005;
+    private static final double kOutboardI = 0.0;
+    private static final double kOutboardD = 0.0;// 0.0005;
     private static final int kCurrentLimit = 30;
     private static final int kCanId = 2;
-    private static final double kMaxJerk = 5;
-    private static final double kMaxAccel = 5;
-    private static final double kMaxVelocity = 5;
+    private static final double kMaxJerkRad_S3 = 2;// 5;
+    private static final double kMaxAccelRad_S2 = 2;// 5;
+    private static final double kMaxVelocityRad_S = 2;// 5;
     private static final double kGearRatio = 55;
+
     private final SupplierLogger m_logger;
-    private final GravityServo ampAngleServo;
+    private final GravityServo m_ampAngleServo;
 
     public AmpPivot(SupplierLogger parent) {
         m_logger = parent.child(this);
@@ -52,20 +63,25 @@ public class AmpPivot extends SubsystemBase implements Glassy {
 
         switch (Identity.instance) {
             case COMP_BOT:
+                Feedforward100 ff = Feedforward100.makeArmPivot();
+                PIDConstants pid = new PIDConstants(kOutboardP, kOutboardI, kOutboardD);
                 CANSparkMotor motor = new NeoCANSparkMotor(
                         m_logger,
                         kCanId,
                         MotorPhase.FORWARD,
                         kCurrentLimit,
-                        Feedforward100.makeNeo(),
-                        new PIDConstants(0, 0, 0));
+                        ff,
+                        pid);
                 RotaryMechanism mech = new RotaryMechanism(
                         motor,
                         new CANSparkEncoder(m_logger, motor),
                         kGearRatio);
                 RotaryPositionSensor encoder = new AS5048RotaryPositionSensor(
-                        m_logger, 3, 0.645439, EncoderDrive.INVERSE);
-                ampAngleServo = new GravityServo(
+                        m_logger,
+                        3,
+                        0.645439,
+                        EncoderDrive.INVERSE);
+                m_ampAngleServo = new GravityServo(
                         mech,
                         m_logger,
                         controller,
@@ -84,7 +100,7 @@ public class AmpPivot extends SubsystemBase implements Glassy {
                         kGearRatio);
                 RotaryPositionSensor simEncoder = new SimulatedRotaryPositionSensor(
                         m_logger, simMech);
-                ampAngleServo = new GravityServo(
+                m_ampAngleServo = new GravityServo(
                         simMech,
                         m_logger,
                         controller,
@@ -95,30 +111,30 @@ public class AmpPivot extends SubsystemBase implements Glassy {
     }
 
     private Profile100 getProfile() {
-        Profile100 fast = new TrapezoidProfile100(kMaxVelocity, kMaxAccel, 0.05);
-        if (Experiments.instance.enabled(Experiment.FancyProfile)) {
-            Profile100 slow = new TrapezoidProfile100(0.1 * kMaxVelocity, 0.1 * kMaxAccel, 0.05);
-            Dashpot d = new Dashpot(fast, slow, 0.2);
-            return new JerkLimiter(d, kMaxJerk);
-        }
-        return fast;
+        Profile100 normal = new TrapezoidProfile100(kMaxVelocityRad_S, kMaxAccelRad_S2, 0.05);
+        Profile100 slow = new TrapezoidProfile100(0.1 * kMaxVelocityRad_S, 0.1 * kMaxAccelRad_S2, 0.05);
+        Profile100 novel = new Dashpot(normal, slow, 0.5);
+        return new SelectableProfile100(
+                () -> Experiments.instance.enabled(Experiment.FancyProfile),
+                novel,
+                normal);
     }
 
     public void setAmpPosition(double value) {
-        ampAngleServo.setPosition(value);
+        m_ampAngleServo.setPosition(value);
     }
 
     /** Zeros controller errors, sets setpoint to current position. */
     public void reset() {
-        ampAngleServo.reset();
+        m_ampAngleServo.reset();
     }
 
     public void stop() {
-        ampAngleServo.stop();
+        m_ampAngleServo.stop();
     }
 
     public OptionalDouble getPositionRad() {
-        return ampAngleServo.getPositionRad();
+        return m_ampAngleServo.getPositionRad();
     }
 
     public Optional<Boolean> inPosition() {
@@ -128,6 +144,11 @@ public class AmpPivot extends SubsystemBase implements Glassy {
         return Optional.of(
                 position.getAsDouble() < 0.75 * Math.PI
                         && position.getAsDouble() > .5 * Math.PI);
+    }
+
+    @Override
+    public void periodic() {
+        m_ampAngleServo.periodic();
     }
 
     @Override
