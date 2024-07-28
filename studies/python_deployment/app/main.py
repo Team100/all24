56b41acabde4e4fb1a5fcb4180f86611ddf345e7
Joel Truher <joel@truher.org>
2024-07-27 17:45:26 -0700

    one more change

--- studies/python_deployment/app/main.py
+++ studies/python_deployment/app/main.py
@@ -5,6 +5,7 @@ on network tables.
 """
 
 from app.camera import Camera, Factory, Request, Size
+from app.display import Display
 from app.identity import Identity
 from app.network import Network
 from app.tag_detector import TagDetector
@@ -16,8 +17,11 @@ def main() -> None:
     identity: Identity = Identity.get()
     camera: Camera = Factory.get(identity)
     size: Size = camera.get_size()
-    tag_detector: TagDetector = TagDetector(identity, size.width, size.height, camera)
+    display: Display = Display(size.width, size.height)
     network: Network = Network(identity)
+    tag_detector: TagDetector = TagDetector(
+        identity, size.width, size.height, camera, display, network
+    )
 
     camera.start()
     try:
@@ -28,7 +32,7 @@ def main() -> None:
             capture_end: int = Timer.time_ns()
             # capture time is how long we wait for the camera, it should be close to zero.
             capture_time_ms: int = (capture_end - capture_start) // 1000000
-            network.log_capture_time(capture_time_ms)
+            network.vision_capture_time_ms.set(capture_time_ms)
             try:
                 tag_detector.analyze(request)
             finally:
