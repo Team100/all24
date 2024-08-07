79dd4baaa177b52f1bcda3ae8a25abd3a5ce3afc
Joel Truher <joel@truher.org>
2024-08-07 15:52:40 -0700

    try the new thing on the simulated data -- it does not work, needs scaling or normalization

---
+++ studies/java_rbf/src/test/java/org/team100/math/LocalizationTest.java
@@ -0,0 +1,147 @@
+package org.team100.math;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class LocalizationTest {
+    record uvdZ(double u, double v, double d, double Z) {
+        @Override
+        public String toString() {
+            return String.format("[%.2f, %.2f, %.2f, %.2f]", u, v, d, Z);
+        }
+    }
+
+    record XY(double X, double Y) {
+        @Override
+        public String toString() {
+            return String.format("[%.2f, %.2f]", X, Y);
+        }
+    }
+
+    static class uvdZAdapter extends RBFInterpolatingFunction.Adapter<uvdZ> {
+
+        @Override
+        int size() {
+            return 4;
+        }
+
+        @Override
+        double[] toArray(uvdZ src) {
+            return new double[] { src.u, src.v, src.d, src.Z };
+        }
+
+        @Override
+        uvdZ fromArray(double[] src) {
+            return new uvdZ(src[0], src[1], src[2], src[3]);
+        }
+
+    }
+
+    static class XYAdapter extends RBFInterpolatingFunction.Adapter<XY> {
+
+        @Override
+        int size() {
+            return 2;
+        }
+
+        @Override
+        double[] toArray(XY src) {
+            return new double[] { src.X, src.Y };
+        }
+
+        @Override
+        XY fromArray(double[] src) {
+            return new XY(src[0], src[1]);
+        }
+
+    }
+
+    static uvdZAdapter xAdapter = new uvdZAdapter();
+    static XYAdapter yAdapter = new XYAdapter();
+
+    @Test
+    void testUvdZXY() throws IOException {
+
+        System.out.println("PATH " + Paths.get(".").toAbsolutePath().normalize().toString());
+
+        double[][] uvdZXY_train = readfile("../../../uvdZXY_train.csv");
+        double[][] uvdZXY_test = readfile("../../../uvdZXY_test.csv");
+
+        // independent variables
+        List<uvdZ> uvdZ_train = getuvdZ(uvdZXY_train);
+        List<uvdZ> uvdZ_test = getuvdZ(uvdZXY_test);
+        // dependent variables
+        List<XY> XY_train = getXY(uvdZXY_train);
+        List<XY> XY_test = getXY(uvdZXY_test);
+
+        RBFInterpolatingFunction<uvdZ, XY> rbf = new RBFInterpolatingFunction<>(
+                uvdZ_train,
+                XY_train,
+                xAdapter,
+                yAdapter,
+                RBFInterpolatingFunction.GAUSSIAN);
+
+        double errSum = 0;
+        for (int i = 0; i < XY_test.size(); ++i) {
+            XY predictedXY = rbf.apply(uvdZ_test.get(i));
+
+            double errX = predictedXY.X - XY_test.get(i).X;
+            double errY = predictedXY.Y - XY_test.get(i).Y;
+
+            double err = Math.hypot(errX, errY);
+            errSum += err;
+            System.out.printf("X %6.3f predictedX %6.3f Y %6.3f predictedY %6.3f\n",
+                    XY_test.get(i).X, predictedXY.X, XY_test.get(i).Y, predictedXY.Y);
+        }
+        double mae = errSum / XY_test.size();
+        System.out.printf("mae %5.3f\n", mae);
+
+    }
+
+    private static double[][] readfile(String filename) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            List<double[]> rows = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(" ");
+                double[] dvals = Arrays.stream(values).mapToDouble(Double::parseDouble).toArray();
+                rows.add(dvals);
+            }
+            double[][] result = rows.stream().toArray(double[][]::new);
+
+            System.out.printf("%s rows: %d\n", filename, result.length);
+            System.out.printf("%s columns: %d\n", filename, result[0].length);
+            return result;
+        }
+    }
+
+    private static List<uvdZ> getuvdZ(double[][] uvdZXY) {
+        List<uvdZ> result = new ArrayList<>();
+        for (int i = 0; i < uvdZXY.length; ++i) {
+            double[] src = uvdZXY[i];
+            uvdZ e = new uvdZ(src[0], src[1], src[2], src[3]);
+            System.out.println(e);
+            result.add(e);
+        }
+        return result;
+    }
+
+    private static List<XY> getXY(double[][] uvdZXY) {
+        List<XY> result = new ArrayList<>();
+        for (int i = 0; i < uvdZXY.length; ++i) {
+            double[] src = uvdZXY[i];
+            XY e = new XY(src[4], src[5]);
+            System.out.println(e);
+            result.add(e);
+        }
+        return result;
+    }
+
+}
