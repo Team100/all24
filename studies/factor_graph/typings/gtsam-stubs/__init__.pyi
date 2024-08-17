ab9021b5e09a3e6c4b447eb637982146b68bd609
Joel Truher <joel@truher.org>
2024-08-16 20:40:14 -0700

    gtsam python stubs

---
+++ studies/factor_graph/typings/gtsam-stubs/__init__.pyi
@@ -0,0 +1,3 @@
+from .gtsam import *
+from . import utils as utils
+from .utils import findExampleDataFile as findExampleDataFile
---
+++ studies/factor_graph/typings/gtsam-stubs/utils.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+import glob as glob
+import posixpath as osp
+__all__ = ['findExampleDataFile', 'glob', 'osp']
+def findExampleDataFile(name):
+    """
+    
+        Find the example data file specified by `name`.
+        
+    """
