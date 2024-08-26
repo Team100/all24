9b1b8621735c9f5b31a02bba90f2efc55d0d1ead
Joel Truher <joel@truher.org>
2024-08-26 08:33:53 -0700

    finally got the boundary-factor jacobian right, looks good

--- studies/factor_graph/factor_graph5.py
+++ studies/factor_graph/factor_graph5.py
@@ -13,31 +13,20 @@ PAUSE_TIME = 1.0
 ANGLE_SCALE = 0.1
 LINEAR_SCALE = 0.2
 
-# noise is really high to make the animation more interesting
+NOISE1 = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.01]))
 NOISE2 = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1]))
 NOISE3 = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.1]))
 
 BOUNDARY = True
 
 
-# the BoundingConstraint is not available in python, I think because it
-# relies on subclasses implementing the "value()" method.
-# so use the Custom Factor idea.
+# this is cut-and-paste from here to figure out custom factors.
 # https://github.com/borglab/gtsam/blob/develop/python/CustomFactors.md
-#
-# this is cut-and-paste from the example in that doc.
 #
 def custom_between_factor(expectation: gtsam.Pose2):
     def error_func(
         this: gtsam.CustomFactor, v: gtsam.Values, H: list[np.ndarray]
     ) -> np.ndarray:
-        """
-        Error function that mimics a BetweenFactor
-        :param this: reference to the current CustomFactor being evaluated
-        :param v: Values object
-        :param H: list of references to the Jacobian arrays
-        :return: the non-linear error
-        """
         key0 = this.keys()[0]
         key1 = this.keys()[1]
         gT1, gT2 = v.atPose2(key0), v.atPose2(key1)
@@ -53,70 +42,28 @@ def custom_between_factor(expectation: gtsam.Pose2):
 
 
 # limit motion to the "field" boundary.
-# see simulated2DConstraints.h:ScalarCoordConstraint1
-# note this constrains *tangent space* which isn't right
+# (can't use BoundingConstraint because it needs a subclass to implement value())
+# this is a 1d constraint on a 3d state (pose2) so it has a 1d noise model and 1x3 jacobian
+# see https://gtsam.org/tutorials/intro.html
 def custom_boundary_constraint():
     def error_func(
         this: gtsam.CustomFactor, v: gtsam.Values, H: list[np.ndarray]
     ) -> np.ndarray:
+        boundary_x = 4.5
         # there's just one key, because we use it with a length-1 keyvector.
         key0 = this.keys()[0]
         gT1 = v.atPose2(key0)
 
-        if gT1.x() <= 4.5:
+        if gT1.x() <= boundary_x:
             # constraint is not active.
             if H is not None:
-                H[0] = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-            return np.array([0, 0.0, 0.0])
+                H[0] = np.array([[0, 0, 0]])
+            return np.array([0])
 
         # constraint is active
-
-        ################
-        # this is from Pose2::range()
-        # jacobian has only x component
-
-        d = np.array([gT1.x() - 4.5, 0, 0])
-        D_d_pose = np.array(
-            [
-                [gT1.rotation().c(), -gT1.rotation().s(), 0],
-                [0, 0, 0],
-                [0, 0, 0],
-            ]
-        )
         if H is not None:
-            H[0] = D_d_pose
-            # print(H[0])
-        error = d.dot(D_d_pose)
-        print(error)
-        return error
-
-        ###############
-        # this works better than the previous attempt
-        # but it is still wrong; this pose should not have
-        # any authority in y.
-
-        # pose_at_boundary = gtsam.Pose2(4.5, gT1.y(), gT1.theta())
-        # # zero error at the boundary
-        # expectation = gtsam.Pose2()
-        # twist = gT1.between(pose_at_boundary)
-        # # print(twist)
-        # error = expectation.localCoordinates(twist)
-        # # print(error)
-
-        # if H is not None:
-        #     H[0] = -twist.inverse().AdjointMap()
-        #     print("H ", H[0])
-        # print("ERROR ", error)
-        # return error
-
-        ###########33
-        # this is fron awhile ago
-        #
-        # error = gT1.x() - 4.5
-        # if H is not None:
-        #     # this is wrong, it needs to be rotated
-        #     H[0] = np.array([[1000, 0, 0], [0, 0, 0], [0, 0, 0]])
-        # return np.array([1000*error, 0.0, 0.0])
+            H[0] = np.array([[gT1.rotation().c(), -gT1.rotation().s(), 0]])
+        return np.array([gT1.x() - boundary_x])
 
     return error_func
 
@@ -169,24 +116,10 @@ def add_odometry_and_target_sights(
         timestamps.insert((l.symbol, x_i))
 
     # also add boundary factor
-    #
-    # Constrained.MixedSignal says there is some (low) uncertainty about the
-    # constrained cartesian dimensions, but doesn't say anything about rotation.
-    #
-    # Constrained.All means that the error produced by our custom factor is not scaled
-    # by sigma at all.  see NoiseModel.cpp.  this seems wrong.
-    #
-    # the Unit noise model seems very soft, not useful.
-    #
-    # the NOISE3 model seems inappropriate for a boundary.
     if BOUNDARY:
         graph.add(
             gtsam.CustomFactor(
-                # the boundary affects translation but not rotation
-                gtsam.noiseModel.Constrained.MixedSigmas(1, np.array([0.2, 0.2, 0.0])),
-                # gtsam.noiseModel.Constrained.All(3),
-                # gtsam.noiseModel.Unit.Create(3),
-                # NOISE3,
+                NOISE1,
                 gtsam.KeyVector([X(x_i)]),
                 custom_boundary_constraint(),
             )
