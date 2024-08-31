fc5b604e8354576eb8f8d9dceaf2804012f0991d
Joel Truher <joel@truher.org>
2024-08-31 13:57:48 -0700

    all point3 numeric differentiation tests work

--- studies/factor_graph/test_point3.py
+++ studies/factor_graph/test_point3.py
@@ -8,10 +8,19 @@ import numpy as np
 from gtsam import Point3  # really np.array
 from numpy.testing import assert_allclose
 
+from numerical_derivative import numericalDerivative11DoublePoint3
+from numerical_derivative import numericalDerivative11Point3Point3
 from numerical_derivative import numericalDerivative21DoublePoint3Point3
 from numerical_derivative import numericalDerivative22DoublePoint3Point3
 from numerical_derivative import numericalDerivative21Point3Point3Point3
 from numerical_derivative import numericalDerivative22Point3Point3Point3
+
+
+def norm_proxy(point: Point3) -> float:
+    return np.linalg.norm(point)
+
+def testFunc( P: Point3,   Q: Point3) -> float:
+    return distance3(P, Q)
 
 
 def dot(p: Point3, q: Point3, H: list[np.array] = None) -> float:
@@ -32,6 +41,43 @@ def cross(p: Point3, q: Point3, H: list[np.array] = None) -> Point3:
 
 def skewSymmetric(wx: float, wy: float, wz: float) -> np.array:
     return np.array([[0.0, -wz, +wy], [+wz, 0.0, -wx], [-wy, +wx, 0.0]])
+
+def  normalize(  p: Point3,  H:list[np.array] = None) -> Point3:
+    """OptionalJacobian<3, 3> H"""
+    normalized: Point3 = p / np.linalg.norm(p)
+    if H is not None:
+        # 3*3 Derivative
+        x2: float = p[0] * p[0]
+        y2: float = p[1] * p[1]
+        z2: float = p[2] * p[2]
+        xy: float = p[0] * p[1]
+        xz: float = p[0] * p[2]
+        yz: float = p[1] * p[2]
+        H[0] = np.array([[ y2 + z2, -xy, -xz], [-xy, x2 + z2, -yz], [-xz, -yz, x2 + y2]])
+        H[0] /= pow(x2 + y2 + z2, 1.5);
+    
+    return normalized;
+
+
+def norm3(  p: Point3,  H: list[np.array] = None) -> float:
+    """OptionalJacobian<1, 3> H"""
+    r:float = math.sqrt(p[0] * p[0] + p[1] * p[1] + p[2] * p[2]);
+    if H is not None:
+        if abs(r) > 1e-10:
+            H[0]  =np.array([[p[0] / r, p[1] / r, p[2] / r]])
+        else:
+            H[0] = np.array([[1, 1, 1]])  # really infinity, why 1 ?
+    return r
+
+def distance3(p1: Point3,   q: Point3, H: list[np.array] = None) -> float:
+    """OptionalJacobian<1, 3> H1, H2"""
+    d: float = np.linalg.norm(q - p1)
+    if H is not None:
+        H[0] = np.array([[ p1[0] - q[0], p1[1] - q[1], p1[2] - q[2]]])
+        H[0] = H[0] *(1. / d);
+        H[1] = np.array([[ -(p1[0]) + q[0], -(p1[1]) + q[1], -(p1[2]) + q[2]]])
+        H[1] = H[1] *(1. / d);
+    return d;
 
 
 class TestPoint3(unittest.TestCase):
@@ -106,6 +152,39 @@ class TestPoint3(unittest.TestCase):
         cross(p, r, H)
         assert_allclose(numericalDerivative21Point3Point3Point3(f, p, r), H[0])
         assert_allclose(numericalDerivative22Point3Point3Point3(f, p, r), H[1])
+
+
+    def test_normalize(self):
+        actualH = [np.zeros((3,3))]
+        point = Point3(1, -2, 3) # arbitrary point
+        expected = Point3(point / math.sqrt(14.0))
+        assert_allclose(expected, normalize(point, actualH))
+        def fn(p: Point3) ->Point3:
+            return normalize(p)
+        expectedH = numericalDerivative11Point3Point3(fn, point);
+        assert_allclose(expectedH, actualH[0])
+
+
+    def test_norm(self):
+        actualH = [np.zeros((3,3))]
+        point = Point3(3,4,5) # arbitrary point
+        expected:float = math.sqrt(50);
+        self.assertAlmostEqual(expected, norm3(point, actualH))
+        expectedH = numericalDerivative11DoublePoint3(norm_proxy, point);
+        assert_allclose(expectedH, actualH[0])
+
+
+    def test_distance(self):
+        P = Point3(1., 12.8, -32.)
+        Q = Point3(52.7, 4.9, -13.3)
+        H = [np.zeros((3,3)), np.zeros((3,3))]
+        d: float = distance3(P, Q, H)
+        expectedDistance:float = 55.5426863;
+        numH1 = numericalDerivative21DoublePoint3Point3(testFunc, P, Q);
+        numH2 = numericalDerivative22DoublePoint3Point3(testFunc, P, Q);
+        self.assertAlmostEqual(expectedDistance, d)
+        assert_allclose(numH1, H[0])
+        assert_allclose(numH2, H[1])
 
 
 if __name__ == "__main__":
