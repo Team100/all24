7f8939f0b8e7b555fcb1687942be88098b14ac76
Joel Truher <joel@truher.org>
2024-08-30 17:12:22 -0700

    working on numerical derivatives

---
+++ studies/factor_graph/numerical_derivative.py
@@ -0,0 +1,67 @@
+# pylint: disable=C0103,C0114,C0116,R0913
+# see numericalDerivative.h
+
+# i couldn't figure out how to make the wrapper work
+# with the std::function argument so i'm just reimplementing
+# the parts i need.
+
+from typing import Callable
+import numpy as np
+
+
+def VectorLocal(a, b):
+    """See VectorSpace.h, TangentVector.Local is difference."""
+    return b - a
+
+
+def VectorRetract(a, b):
+    """See VectorSpace.h, TangentVector.Retract is sum."""
+    return a + b
+
+
+def numericalGradientVector2(
+    h: Callable[[np.array], float], x: np.array, delta=1e-5
+) -> np.array:
+    N = 2
+    factor = 1.0 / (2.0 * delta)
+    d = np.zeros(N)
+    g = np.zeros(N)
+    for j in range(N):
+        d[j] = delta
+        hxplus: float = h(VectorRetract(x, d))
+        d[j] = -delta
+        hxmin: float = h(VectorRetract(x, d))
+        d[j] = 0
+        g[j] = (hxplus - hxmin) * factor
+    return g
+
+
+def numericalDerivative11VectorDouble(
+    h: Callable[[float], np.array], x: float, delta=1e-5
+) -> np.array:
+    N =1  # for now
+    print(x)
+    print(type(x))
+    hx = h(x)
+    m: int = np.shape(hx)[0]
+
+    dx = np.zeros(N)
+
+    H = np.zeros((m, N))
+    factor: float = 1.0 / (2.0 * delta)
+    for j in range(N):
+        dx[j] = delta
+        dy1 = VectorLocal(hx, h(VectorRetract(x, dx)))
+        dx[j] = -delta
+        dy2 = VectorLocal(hx, h(VectorRetract(x, dx)))
+        dx[j] = 0
+        H[:, j] = (dy1 - dy2) * factor
+    return H
+
+
+def numericalDerivative61Vector6DoubleDoubleDoubleDoubleDoubleDouble(
+    h, x1, x2, x3, x4, x5, x6, delta=1e-5
+):
+    return numericalDerivative11VectorDouble(
+        lambda a1: h(a1, x2, x3, x4, x5, x6), x1, delta
+    )
--- studies/factor_graph/test_numerical_derivative.py
+++ studies/factor_graph/test_numerical_derivative.py
@@ -1,14 +1,62 @@
-# pylint: disable=no-member
+# pylint: disable=no-member,C0103,C0114,C0115,C0116,R0913
 
-# can i get the numerical derivative c++ code to work via python?
+# see testNumericalDerivative.cpp
 
-import gtsam
+
+import math
+import unittest
 import numpy as np
+from numpy.testing import assert_allclose
+import gtsam
 
-x1 = gtsam.Point2(0,0)
-print(type(x1)) # this is ndarray because Point2 is really Eigen::Vector2d
-x2 = gtsam.Point2(1,1)
-d = np.linalg.norm(x1-x2)
-print(d)
+from numerical_derivative import (
+    numericalGradientVector2,
+    numericalDerivative61Vector6DoubleDoubleDoubleDoubleDoubleDouble,
+)
 
 
+def f(x: np.array) -> float:
+    assert x.size == 2
+    return math.sin(x[0]) + math.cos(x[1])
+
+
+def f6(x1: float, x2: float, x3: float, x4: float, x5: float, x6: float) -> np.array:
+    print(type(x1))
+    return np.array(
+        [
+            math.sin(x1),
+            math.cos(x2),
+            x3 * x3,
+            x4 * x4 * x4,
+            math.sqrt(x5),
+            math.sin(x6) - math.cos(x6),
+        ]
+    )
+
+
+class TestNumericalDerivative(unittest.TestCase):
+
+    def test_numericalGradient(self):
+        x = np.array([1.0, 1.1])
+        expected = np.array([math.cos(x[0]), -math.sin(x[1])])
+        actual: gtsam.Vector = numericalGradientVector2(f, x)
+        assert_allclose(expected, actual, atol=1e-5)
+
+    def test_numeriDerivative61(self):
+        x1 = 1
+        x2 = 2
+        x3 = 3
+        x4 = 4
+        x5 = 5
+        x6 = 6
+        expected61 = np.array([[math.cos(x1)], [0], [0], [0], [0], [0]])
+        actual61 = numericalDerivative61Vector6DoubleDoubleDoubleDoubleDoubleDouble(
+            f6, x1, x2, x3, x4, x5, x6
+        )
+        print("expected ", expected61)
+        print("actual ", actual61)
+        assert_allclose(expected61, actual61, atol=1e-5)
+
+
+if __name__ == "__main__":
+    unittest.main()
