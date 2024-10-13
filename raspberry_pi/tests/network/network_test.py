7dd63434fd9a7c7b14ddeb6e68c02fd0e82c73b3
Joel Truher <joel@truher.org>
2024-10-13 15:50:20 -0700

    sigh

---
+++ raspberry_pi/tests/network/network_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from app.config.identity import Identity
+from app.network.fake_network import FakeNetwork, doubles
+
+
+class NetworkTest(unittest.TestCase):
+    def test_send(self) -> None:
+        network = FakeNetwork()
+        sender = network.get_double_sender("foo")
+        sender.send(1.0, 0)
+        self.assertEqual(1.0, doubles["foo"][0])
