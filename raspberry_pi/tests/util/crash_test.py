56396148972c68a76c0a5c7f2d35d446e0b3b80b
Joel Truher <joel@truher.org>
2024-10-13 21:35:31 -0700

    another crash test

--- raspberry_pi/tests/util/crash_test.py
+++ raspberry_pi/tests/util/crash_test.py
@@ -5,7 +5,13 @@ from wpimath.geometry import Rotation3d
 
 
 class CrashTest(unittest.TestCase):
-    def test_crash(self) -> None:
+    def test_crash1(self) -> None:
+        rotation = Rotation3d()
+        self.assertEqual(0, rotation.X())
+        self.assertEqual(0, rotation.Y())
+        self.assertEqual(0, rotation.Z())
+
+    def test_crash2(self) -> None:
         initial = np.array([1, 0, 0], dtype=np.float64)
         final = np.array([1, 0, 0], dtype=np.float64)
         rotation = Rotation3d(initial=initial, final=final)
