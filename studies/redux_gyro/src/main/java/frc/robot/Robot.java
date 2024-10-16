ef208ce5849221ac52c7bc912aeee39f32dcd648
Joel Truher <joel@truher.org>
2024-10-16 16:40:54 -0700

    redux robotics gyro study

---
+++ studies/redux_gyro/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,95 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.reduxrobotics.sensors.canandgyro.Canandgyro;
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  Canandgyro gyro = new Canandgyro(0);
+
+
+  private RobotContainer m_robotContainer;
+
+
+  @Override
+  public void robotInit() {
+    m_robotContainer = new RobotContainer();
+    DataLogManager.start();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void disabledExit() {
+  }
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void autonomousExit() {
+  }
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double theta = gyro.getYaw();
+    double omega = gyro.getAngularVelocityYaw();
+    SmartDashboard.putNumber("theta", theta);
+    SmartDashboard.putNumber("omega", omega);
+  }
+
+  @Override
+  public void teleopExit() {
+  }
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void testExit() {
+  }
+}
