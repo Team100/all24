a9873f182b82e8893cc50e21cc22c725786abea4
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2024-10-16 22:50:02 -0700

    Vision work

--- raspberry_pi/app/camera/interpreter_factory.py
+++ raspberry_pi/app/camera/interpreter_factory.py
@@ -3,23 +3,29 @@ from typing import Protocol
 from app.camera.interpreter_protocol import Interpreter
 from app.config.identity import Identity
 from app.camera.camera_protocol import Camera
-from app.dashboard.display import Display
+from app.dashboard.real_display import RealDisplay
 from app.network.real_network import Network
 from app.localization.note_detector import NoteDetector
 from app.localization.tag_detector import TagDetector
 
-class interpreter_factory():
+class InterpreterFactory():
     
     @staticmethod
-    def get(self, 
-            identity: Identity, 
+    def get(identity: Identity, 
             cam: Camera,
             camera_num: int,
-            display: Display,
-            network: Network,) -> Interpreter:
+            network: Network) -> Interpreter:
+        size = cam.get_size()
+        if identity != Identity.UNKNOWN:
+            scale = 0.25
+        else:
+            scale = 1.0
         match identity: 
             case (Identity.GLOBAL_GAME_PIECE
-                  |Identity.GAME_PIECE):
+                | Identity.GAME_PIECE):
+                display = RealDisplay(
+                    int(scale * size.width), int(scale * size.height), "tag" + str(camera_num)
+                )
                 return NoteDetector(identity, cam, camera_num, display, network)
             case (Identity.RIGHTAMP
                 | Identity.LEFTAMP
@@ -27,6 +33,9 @@ class interpreter_factory():
                 | Identity.GLOBAL_LEFT
                 | Identity.GLOBAL_RIGHT
                 | Identity.DEV):
+                display = RealDisplay(
+                    int(scale * size.width), int(scale * size.height), "note" + str(camera_num)
+                )
                 return TagDetector(identity, cam, camera_num, display, network)
 
 
--- raspberry_pi/app/main.py
+++ raspberry_pi/app/main.py
@@ -12,6 +12,7 @@ use the script called "runapp.py" in the raspberry_pi directory
 from threading import Event, Thread
 
 from app.camera.camera_factory import CameraFactory
+from app.camera.interpreter_factory import InterpreterFactory
 from app.camera.camera_loop import CameraLoop
 from app.config.identity import Identity
 from app.dashboard.real_display import RealDisplay
@@ -33,16 +34,17 @@ def main() -> None:
         loops: list[Looper] = []
 
         camera0 = CameraFactory.get(identity, 0)
-        size0 = camera0.get_size()
-        if identity != Identity.UNKNOWN:
-            scale = 0.25
-        else:
-            scale = 1.0
-        display0 = RealDisplay(
-            int(scale * size0.width), int(scale * size0.height), "tag0"
-        )
+        # size0 = camera0.get_size()
+        # if identity != Identity.UNKNOWN:
+        #     scale = 0.25
+        # else:
+        #     scale = 1.0
+        # display0 = RealDisplay(
+        #     int(scale * size0.width), int(scale * size0.height), "tag0"
+        # )
         # display01 = RealDisplay(size0.width, size0.height, "note0")
-        detector0 = TagDetector(identity, camera0, 0, display0, network)
+        detector0 = InterpreterFactory.get(identity, camera0, 0, network)
+        # detector0 = TagDetector(identity, camera0, 0, display0, network)
         # detector01 = NoteDetector(identity, camera0, 1, display01, network)
         # loops.append(CameraLoop(camera0, [detector0, detector01], done))
         loops.append(CameraLoop(camera0, [detector0], done))
@@ -51,12 +53,13 @@ def main() -> None:
         #
         if CameraFactory.get_num_cameras(identity) > 1:
             camera1 = CameraFactory.get(identity, 1)
-            size1 = camera1.get_size()
-            display1 = RealDisplay(
-                int(scale * size1.width), int(scale * size1.height), "note1"
-            )
+            # size1 = camera1.get_size()
+            # display1 = RealDisplay(
+            #     int(scale * size1.width), int(scale * size1.height), "note1"
+            # )
             # display11 = RealDisplay(size1.width, size1.height, "tag1")
-            detector1 = NoteDetector(identity, camera1, 2, display1, network)
+            # detector1 = NoteDetector(identity, camera1, 2, display1, network)
+            detector1 = InterpreterFactory.get(identity, camera1, 1, network)
             # detector11 = TagDetector(identity, camera1, 3, display11, network)
             # loops.append(CameraLoop(camera1, [detector1, detector11], done))
             loops.append(CameraLoop(camera1, [detector1], done))
