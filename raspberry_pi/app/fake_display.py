68f0e2a6254962bb055e95e4116f1ad86526c37a
Dobromir Montauk <dobromir.montauk@mashgin.com>
2024-10-19 09:59:40 -0700

    First try

---
+++ raspberry_pi/app/fake_display.py
@@ -0,0 +1,12 @@
+class Display:
+    def __init__(self, width, height, camera_num) -> None:
+        pass
+
+    def draw_result(self, image, result_item, pose) -> None:
+        pass
+
+    def draw_text(self, image, msg, loc) -> None:
+        pass
+
+    def put_frame(self, img) -> None:
+        pass
