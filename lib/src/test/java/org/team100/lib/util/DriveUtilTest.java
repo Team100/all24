44d596584338a23f906005b11771697f8be85d18
Joel Truher <joel@truher.org>
2024-10-22 22:17:16 -0700

    hm modules

--- lib/src/test/java/org/team100/lib/util/DriveUtilTest.java
+++ lib/src/test/java/org/team100/lib/util/DriveUtilTest.java
@@ -1,9 +1,15 @@
 package org.team100.lib.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.team100.lib.hid.DriverControl;
+import org.team100.lib.motion.drivetrain.kinodynamics.SwerveDriveKinematics100;
+import org.team100.lib.motion.drivetrain.kinodynamics.SwerveModulePosition100;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Twist2d;
 
 class DriveUtilTest {
     private static final double kDelta = 0.001;
@@ -32,6 +38,24 @@ class DriveUtilTest {
             assertEquals(0.5, actual.x(), kDelta);
             assertEquals(0.5, actual.y(), kDelta);
         }
+    }
+
+    @Test
+    void testRoundTripModuleDeltas() {
+        // i'm not sure the module delta calculation is correct.
+        Twist2d t = new Twist2d();
+        Translation2d m_fl = new Translation2d(12, 12);
+        Translation2d m_fr = new Translation2d(12, -12);
+        Translation2d m_bl = new Translation2d(-12, 12);
+        Translation2d m_br = new Translation2d(-12, -12);
+
+        SwerveDriveKinematics100 m_kinematics = new SwerveDriveKinematics100(m_fl, m_fr, m_bl, m_br);
+
+        SwerveModulePosition100[] p = m_kinematics.toSwerveModulePosition(t);
+
+        assertEquals(0, p[0].distanceMeters, kDelta);
+        assertTrue(p[0].angle.isEmpty());
+
     }
 
 }
