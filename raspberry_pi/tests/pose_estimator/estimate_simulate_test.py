efbe6ee17b77f875e93c30ee6af6cd4731b558e4
Joel Truher <joel@truher.org>
2024-10-26 08:10:21 -0700

    check odo err is zero

--- raspberry_pi/tests/pose_estimator/estimate_simulate_test.py
+++ raspberry_pi/tests/pose_estimator/estimate_simulate_test.py
@@ -1,15 +1,16 @@
 # pylint: disable=C0301,E0611,R0903
 
 import math
+import time
 import unittest
 
 import numpy as np
 from gtsam import Cal3DS2, Point2, Point3, Pose2, Pose3, Rot3
+from gtsam.symbol_shorthand import X
 from wpimath.geometry import Pose2d
 
 from app.pose_estimator.estimate import Estimate
 from tests.pose_estimator.simulator import Simulator
-import time
 
 
 class EstimateSimulateTest(unittest.TestCase):
@@ -19,19 +20,26 @@ class EstimateSimulateTest(unittest.TestCase):
         est.init(sim.wpi_pose)
 
         print()
-        for i in range(1,1000):
+        for i in range(1, 100):
             t0 = time.time_ns()
             time_us = 20000 * i
             est.odometry(time_us, sim.positions)
             est.update()
             t1 = time.time_ns()
-            et = t1-t0
-            print(f"{et/1e9} {est.result.size()}")
-            t = i*0.02
+            et = t1 - t0
+            # print(f"{et/1e9} {est.result.size()}")
+            t = i * 0.02
             gt_x = sim.gt_x
             gt_y = sim.gt_y
             gt_theta = sim.gt_theta
 
-            #print(f"{t:5.2f} {gt_x:5.2f} {gt_y:5.2f} {gt_theta:5.2f}")
-           
+            # using just odometry without noise, the error
+            # is exactly zero, all the time. :-)
+            p: Pose2 = est.result.atPose2(X(time_us))
+            est_x = p.x()
+            est_y = p.y()
+            est_theta = p.theta()
+
+            print(f"{t:7.4f}, {gt_x:7.4f}, {gt_y:7.4f}, {gt_theta:7.4f}, {est_x:7.4f}, {est_y:7.4f}, {est_theta:7.4f}")
+
             sim.step(0.02)
