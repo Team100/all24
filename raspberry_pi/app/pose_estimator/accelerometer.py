4b5e9cf86902fe747540d4025b3c4d4294f3a26c
Joel Truher <joel@truher.org>
2024-10-28 16:12:48 -0700

    accel

--- raspberry_pi/app/pose_estimator/accelerometer.py
+++ raspberry_pi/app/pose_estimator/accelerometer.py
@@ -6,12 +6,21 @@ IMUFactor; this is simpler.
 
 # pylint: disable=C0103,E0611
 import gtsam
-from numpy import double
+import numpy as np
 from gtsam.noiseModel import Base as SharedNoiseModel
 
+def h(p0: gtsam.Pose2, p1: gtsam.Pose2, p2: gtsam.Pose2) -> np.ndarray:
 
-def factor(x: double,
-           y:double,
+def h_H(measured: np.ndarray, p0: gtsam.Pose2, p1: gtsam.Pose2, p2: gtsam.Pose2, H: list[np.ndarray]):
+    result = h(p0, p1, p2) - measured
+    if H is not None:
+        H[0] =
+        H[1] =
+        H[2] =
+    return result
+
+def factor(x: float,
+           y:float,
            model: SharedNoiseModel,
            p0_key: gtsam.Symbol,
            p1_key: gtsam.Symbol,
