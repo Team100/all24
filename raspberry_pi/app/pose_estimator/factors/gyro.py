0b0aa6f4882a9927b6c461b03b756f64af158e76
Joel Truher <joel@truher.org>
2024-11-08 07:25:34 -0800

    use native gyro factor.

--- raspberry_pi/app/pose_estimator/factors/gyro.py
+++ raspberry_pi/app/pose_estimator/factors/gyro.py
@@ -11,9 +11,7 @@ import gtsam
 import numpy as np
 from gtsam.noiseModel import Base as SharedNoiseModel  # type:ignore
 
-from app.pose_estimator.numerical_derivative import (
-    numericalDerivative11
-)
+from app.pose_estimator.numerical_derivative import numericalDerivative11
 
 
 def h(p0: gtsam.Pose2) -> np.ndarray:
@@ -21,9 +19,7 @@ def h(p0: gtsam.Pose2) -> np.ndarray:
     return np.array([p0.rotation().theta()])
 
 
-def h_H(
-    measured: np.ndarray, p0: gtsam.Pose2, H: list[np.ndarray]
-) -> np.ndarray:
+def h_H(measured: np.ndarray, p0: gtsam.Pose2, H: list[np.ndarray]) -> np.ndarray:
     """measured: 1x1 array"""
     result = h(p0) - measured
     if H is not None:
@@ -31,7 +27,7 @@ def h_H(
     return result
 
 
-def factor(
+def factorCustom(
     measured: np.ndarray,
     model: SharedNoiseModel,
     p0_key: int,
@@ -48,3 +44,20 @@ def factor(
         gtsam.KeyVector([p0_key]),  # type:ignore
         error_func,
     )
+
+
+def factorNative(
+    measured: np.ndarray,
+    model: SharedNoiseModel,
+    p0_key: int,
+) -> gtsam.NoiseModelFactor:
+    """See PoseRotationPrior.h"""
+    return gtsam.PoseRotationPrior2D(p0_key, gtsam.Pose2(0, 0, measured[0]), model)
+
+
+def factor(
+    measured: np.ndarray,
+    model: SharedNoiseModel,
+    p0_key: int,
+) -> gtsam.NoiseModelFactor:
+    return factorCustom(measured, model, p0_key)
